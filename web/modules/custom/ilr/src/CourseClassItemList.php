18a3623cbf81d7d1faf2662e9057304391183617
Jeff Amaral <jeff.amaral@cornell.edu>
2019-09-25 11:56:25 -0700

    Add computed upcoming classes backreference field to courses

---
+++ web/modules/custom/ilr/src/CourseClassItemList.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Drupal\ilr;
+
+use Drupal\Core\Field\EntityReferenceFieldItemList;
+use Drupal\Core\TypedData\ComputedItemListTrait;
+use Drupal\Core\Datetime\DrupalDateTime;
+
+/**
+ * Entity reference list for a computed field that displays classes.
+ */
+class CourseClassItemList extends EntityReferenceFieldItemList {
+
+  use ComputedItemListTrait;
+
+  /**
+   *
+   */
+  protected function computeValue() {
+    $course_entity = $this->getEntity();
+
+    $query = \Drupal::entityQuery('node')
+      ->condition('status', 1)
+      ->condition('type', 'class')
+      ->condition('field_course', $course_entity->id())
+      ->sort('field_date_start');
+
+    // Get future start dates only.
+    if (TRUE) {
+      $now = new DrupalDateTime('now');
+      $query->condition('field_date_start', $now->format(DATETIME_DATETIME_STORAGE_FORMAT), '>=');
+    }
+
+    $results = $query->execute();
+    $key = 0;
+
+    foreach ($results as $nid) {
+      $this->list[$key] = $this->createItem($key, $nid);
+      $key++;
+    }
+  }
+
+}
---
+++ web/modules/custom/ilr/src/FieldStorageDefinition.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Drupal\ilr;
+
+use Drupal\Core\Field\BaseFieldDefinition;
+
+/**
+ * A custom field storage definition class.
+ *
+ * For convenience we extend from BaseFieldDefinition although this should not
+ * implement FieldDefinitionInterface.
+ *
+ * Copied from core/modules/system/tests/modules/entity_test/src/FieldStorageDefinition.php
+ *
+ * @todo Provide and make use of a proper FieldStorageDefinition class instead:
+ *   https://www.drupal.org/node/2280639.
+ */
+class FieldStorageDefinition extends BaseFieldDefinition {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function isBaseField() {
+    return FALSE;
+  }
+
+}
