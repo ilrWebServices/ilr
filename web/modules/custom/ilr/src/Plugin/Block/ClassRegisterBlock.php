a448ffd5b493a9f0805be51685cbffa2cf6cbe95
Aaron Froehlich <aaronfroehlich@gmail.com>
2019-10-15 16:08:33 -0400

    Add registration buttons to the class register block
    These get progressively enhanced via js in union registration forms.

--- web/modules/custom/ilr/src/Plugin/Block/ClassRegisterBlock.php
+++ web/modules/custom/ilr/src/Plugin/Block/ClassRegisterBlock.php
@@ -3,6 +3,9 @@
 namespace Drupal\ilr\Plugin\Block;
 
 use Drupal\Core\Block\BlockBase;
+use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
+use Drupal\Core\Entity\EntityManagerInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
  * Provides a 'ClassRegisterBlock' block.
@@ -12,7 +15,43 @@ use Drupal\Core\Block\BlockBase;
  *  admin_label = @Translation("Class register"),
  * )
  */
-class ClassRegisterBlock extends BlockBase {
+class ClassRegisterBlock extends BlockBase implements ContainerFactoryPluginInterface {
+
+  /**
+   * The entity manager.
+   *
+   * @var Drupal\Core\Entity\EntityManagerInterface;
+   */
+  protected $entityManager;
+
+  /**
+   * Constructs a new ClassRegisterBlock instance.
+   *
+   * @param array $configuration
+   *   A configuration array containing information about the plugin instance.
+   * @param string $plugin_id
+   *   The plugin_id for the plugin instance.
+   * @param mixed $plugin_definition
+   *   The plugin implementation definition.
+   * @param $entity_manager
+   *   The entity manager object.
+   */
+  public function __construct(array $configuration, $plugin_id, $plugin_definition, EntityManagerInterface $entity_manager) {
+    parent::__construct($configuration, $plugin_id, $plugin_definition);
+    $this->entityManager = $entity_manager;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
+    return new static(
+      $configuration,
+      $plugin_id,
+      $plugin_definition,
+      $container->get('entity.manager')
+    );
+  }
 
   /**
    * {@inheritdoc}
@@ -28,9 +67,32 @@ class ClassRegisterBlock extends BlockBase {
       return $build;
     }
 
+    $classes = $node->classes->referencedEntities();
+
+    $class_info = [];
+    foreach ($classes as $class) {
+      $mapped_objects = $this->entityManager->getStorage('salesforce_mapped_object')
+        ->loadByProperties([
+          'drupal_entity__target_type' => 'node',
+          'drupal_entity__target_id' => $class->id(),
+          'salesforce_mapping' => 'class_node'
+        ]);
+
+      $mapping = reset($mapped_objects);
+
+      if (empty($mapping)) {
+        continue;
+      }
+
+      $class_info[] = [
+        'salesforce_id' => $mapping->salesforce_id->getString(),
+        'entity' => $class
+      ];
+    }
+
     $build = [
       '#theme' => 'ilr_class_register_block',
-      '#classes' => $node->classes->referencedEntities(),
+      '#classes' => $class_info
     ];
 
     return $build;
--- web/sites/default/settings.php
+++ web/sites/default/settings.php
@@ -799,6 +799,7 @@ if (file_exists($app_root . '/' . $site_path . '/settings.platformsh.php')) {
 $config['salesforce.settings']['consumer_key'] = getenv('SALESFORCE_CONSUMER_KEY');
 $config['salesforce.settings']['consumer_secret'] = getenv('SALESFORCE_CONSUMER_SECRET');
 $config['salesforce.settings']['login_url'] = getenv('SALESFORCE_LOGIN_URL');
+$config['ilr_registration_system.settings']['url'] = getenv('REGISTRATION_SYSTEM_URL');
 
 /**
  * Migration data sources.
