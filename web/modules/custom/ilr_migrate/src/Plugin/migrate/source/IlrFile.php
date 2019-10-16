7ce2eb34dd250b6dce029726e4ed00ba2924145a
Jeff Amaral <jeff.amaral@cornell.edu>
2019-10-16 13:12:52 -0700

    Add alt text to migrated image media entities

--- web/modules/custom/ilr_migrate/src/Plugin/migrate/source/IlrFile.php
+++ web/modules/custom/ilr_migrate/src/Plugin/migrate/source/IlrFile.php
@@ -9,6 +9,7 @@ use Drupal\migrate\Row;
  * Drupal 7 file source from database with customizations.
  *
  * - Added file type support.
+ * - Added integration with file_entity for image type alt text.
  *
  * @MigrateSource(
  *   id = "ilr_d7_file",
@@ -34,6 +35,11 @@ class IlrFile extends File {
 
     // Don't import private files. Most of them are for entity forms.
     $query->condition('uri', 'private://%', 'NOT LIKE');
+
+    // Add the image alt value (from a field added via file_entity module) to
+    // the source.
+    $query->leftJoin('field_data_field_file_image_alt_text', 'fia', 'f.fid = fia.entity_id AND fia.bundle = \'image\'');
+    $query->addField('fia', 'field_file_image_alt_text_value', 'image_alt');
 
     return $query;
   }
