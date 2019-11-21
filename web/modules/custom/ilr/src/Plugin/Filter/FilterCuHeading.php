0ed4cbbd5507b47585f9c2c47a4870c73ae0bf74
Jeff Amaral <jeff.amaral@cornell.edu>
2019-11-21 11:51:32 -0800

    Add a custom input filter that adds `cu-heading` to heading elements

---
+++ web/modules/custom/ilr/src/Plugin/Filter/FilterCuHeading.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Drupal\ilr\Plugin\Filter;
+
+use Drupal\filter\Plugin\FilterBase;
+use Drupal\filter\FilterProcessResult;
+
+/**
+ * @Filter(
+ *   id = "filter_cu_heading",
+ *   title = @Translation("CU Heading Filter"),
+ *   description = @Translation("Add the .cu-heading class to HTML heading elements."),
+ *   type = Drupal\filter\Plugin\FilterInterface::TYPE_TRANSFORM_REVERSIBLE,
+ * )
+ */
+class FilterCuHeading extends FilterBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function process($text, $langcode) {
+    $new_text = preg_replace('/<h(\d)>/', '<h$1 class="cu-heading">', $text);
+    return new FilterProcessResult($new_text);
+  }
+
+}
