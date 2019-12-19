f5052d8b2002b09e39a4b7b82c78797f310ab96d
Jeff Amaral <jeff.amaral@cornell.edu>
2019-12-19 08:55:54 -0800

    Add and update comments in CollectionItemController.php

--- web/modules/custom/collection/src/Controller/CollectionItemController.php
+++ web/modules/custom/collection/src/Controller/CollectionItemController.php
@@ -10,12 +10,41 @@ use Drupal\Core\Routing\RouteMatchInterface;
 
 class CollectionItemController extends EntityController {
 
+  /**
+   * Returns a redirect response object for the specified route.
+   *
+   * This is overridden from EntityController because we need to ensure that the
+   * `collection` parameter is included in the redirect response. This method is
+   * only called from the parent of $this->addPage.
+   *
+   * @param string $route_name The name of the route to which to redirect.
+   * @param array $route_parameters (optional) Parameters for the route.
+   * @param array $options (optional) An associative array of additional
+   *   options.
+   * @param int $status (optional) The HTTP redirect status code for the
+   *   redirect. The default is 302 Found.
+   *
+   * @return \Symfony\Component\HttpFoundation\RedirectResponse A redirect
+   *   response object that may be returned by the controller.
+   */
   protected function redirect($route_name, array $route_parameters = [], array $options = [], $status = 302) {
     $options['absolute'] = TRUE;
     $route_parameters['collection'] = \Drupal::routeMatch()->getRawParameter('collection');
     return new RedirectResponse(Url::fromRoute($route_name, $route_parameters, $options)->toString(), $status);
   }
 
+  /**
+   * Displays add links for the available collection item bundles.
+   *
+   * Redirects to the add form if there's only one bundle available.
+   *
+   * @param string $entity_type_id
+   *   The entity type ID.
+   *
+   * @return \Symfony\Component\HttpFoundation\RedirectResponse|array
+   *   If there's only one available bundle, a redirect response.
+   *   Otherwise, a render array with the add links for each bundle.
+   */
   public function addPage($entity_type_id) {
     $build = parent::addPage($entity_type_id);
 
@@ -36,7 +65,7 @@ class CollectionItemController extends EntityController {
   }
 
   /**
-   * Provides a title callback.
+   * Provides a collection item list title callback.
    *
    * @param \Drupal\Core\Routing\RouteMatchInterface $route_match
    *   The route match.
