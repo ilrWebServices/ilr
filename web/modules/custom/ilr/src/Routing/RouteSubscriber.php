a1d481874fa3c7d26fea0da2fcaa4f52a96ae384
Jeff Amaral <jeff.amaral@cornell.edu>
2020-01-30 12:10:39 -0800

    Set the user profile page (currently, the dashboard) to use the admin theme

---
+++ web/modules/custom/ilr/src/Routing/RouteSubscriber.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Drupal\ilr\Routing;
+
+use Drupal\Core\Routing\RouteSubscriberBase;
+use Symfony\Component\Routing\RouteCollection;
+
+/**
+ * Listens to the dynamic route events.
+ */
+class RouteSubscriber extends RouteSubscriberBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function alterRoutes(RouteCollection $collection) {
+    if ($route = $collection->get('entity.user.canonical')) {
+      $route->setOption('_admin_route', TRUE);
+    }
+  }
+
+}
