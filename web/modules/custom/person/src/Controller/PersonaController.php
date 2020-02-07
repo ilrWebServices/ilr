38b0a7ed28066403f31263956928ce8683c69be6
Jeff Amaral <jeff.amaral@cornell.edu>
2020-02-07 11:06:54 -0800

    Fix PersonaController comment blocks

--- web/modules/custom/person/src/Controller/PersonaController.php
+++ web/modules/custom/person/src/Controller/PersonaController.php
@@ -11,21 +11,7 @@ use Drupal\Core\Entity\EntityInterface;
 class PersonaController extends EntityController {
 
   /**
-   * Returns a redirect response object for the specified route.
-   *
-   * This is overridden from EntityController because we need to ensure that the
-   * `person` parameter is included in the redirect response. This method is
-   * only called from the parent of $this->addPage.
-   *
-   * @param string $route_name The name of the route to which to redirect.
-   * @param array $route_parameters (optional) Parameters for the route.
-   * @param array $options (optional) An associative array of additional
-   *   options.
-   * @param int $status (optional) The HTTP redirect status code for the
-   *   redirect. The default is 302 Found.
-   *
-   * @return \Symfony\Component\HttpFoundation\RedirectResponse A redirect
-   *   response object that may be returned by the controller.
+   * {@inheritdoc}
    */
   protected function redirect($route_name, array $route_parameters = [], array $options = [], $status = 302) {
     $options['absolute'] = TRUE;
@@ -34,16 +20,7 @@ class PersonaController extends EntityController {
   }
 
   /**
-   * Displays add links for the available persona bundles.
-   *
-   * Redirects to the add form if there's only one bundle available.
-   *
-   * @param string $entity_type_id
-   *   The entity type ID.
-   *
-   * @return \Symfony\Component\HttpFoundation\RedirectResponse|array
-   *   If there's only one available bundle, a redirect response.
-   *   Otherwise, a render array with the add links for each bundle.
+   * {@inheritdoc}
    */
   public function addPage($entity_type_id) {
     $build = parent::addPage($entity_type_id);
