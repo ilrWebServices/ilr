da771b41a3421b90244a9a95ca9dd16b012c4f1f
Jeff Amaral <jeff.amaral@cornell.edu>
2020-03-12 12:09:43 -0700

    Add non-dismissible custom message to course pages and adjust message style

--- web/modules/custom/ilr/ilr.post_update.php
+++ web/modules/custom/ilr/ilr.post_update.php
@@ -40,3 +40,22 @@ function ilr_post_update_instructor_photo_alt_attributes(&$sandbox) {
     '%modified_media_count' => $modified_media_count
   ]);
 }
+
+/**
+ * Add add a course message block with a given UUID for layout builder.
+ */
+function ilr_post_update_course_message_block(&$sandbox) {
+  $blockEntityManager = \Drupal::service('entity_type.manager')->getStorage('block_content');
+
+  $block = $blockEntityManager->create([
+    'type' => 'simple_text',
+    'uuid' => '280c1d2d-0456-45eb-84dc-d114c5e7b2fa',
+    'info' => 'Course Message',
+    'label_display' => 0,
+  ]);
+
+  $block->body->value = '<p>Effective March 16, 2020, the educational programs scheduled to be delivered at our New York City facility will be moved to virtual delivery, where possible, or rescheduled. For those programs that do not lend themselves to a distance-education format, we will postpone them until we receive guidance from the University and public health officials that it is safe to hold those programs in-person. Please contact <a href="mailto:ilrcustomerservice@cornell.edu">ilrcustomerservice@cornell.edu</a> with any questions. We will provide updates as they become available from the University.</p>';
+  $block->body->format = 'basic_formatting';
+
+  $block->save();
+}
