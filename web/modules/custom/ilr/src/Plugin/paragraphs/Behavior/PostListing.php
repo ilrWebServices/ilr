468e747c3baf7f8f91462a34ed057006acd99600
Jeff Amaral <jeff.amaral@cornell.edu>
2020-04-14 15:26:08 -0700

    Prevent duplicate items in post listings for the same collection
    ...at least for listings displayed on the same request. This won't work for paragraph previews, for example.

--- web/modules/custom/ilr/src/Plugin/paragraphs/Behavior/PostListing.php
+++ web/modules/custom/ilr/src/Plugin/paragraphs/Behavior/PostListing.php
@@ -87,12 +87,17 @@ class PostListing extends ParagraphsBehaviorBase {
     $cache_tags[] = 'node_list';
     // $cache_tags[] = 'node_list:post'; // 8.9 and above
     $posts = [];
+    $dedupe_group = 'dedupe__collection_' . $collection->id();
 
     $query = $collection_item_storage->getQuery();
     $query->condition('collection', $collection->id());
     $query->condition('type', 'blog');
     $query->condition('item.entity:node.status', 1);
     $query->condition('item.entity:node.type', 'post');
+    // Add tags to remove duplicates in similar post_listings. See
+    // ilr_query_post_listing_alter().
+    $query->addTag('post_listing');
+    $query->addTag($dedupe_group);
 
     if ($category_term_id = $paragraph->getBehaviorSetting($this->getPluginId(), 'post_categories')) {
       $category_group = $query->andConditionGroup();
