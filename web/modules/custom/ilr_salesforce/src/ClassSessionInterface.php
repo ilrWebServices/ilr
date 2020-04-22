80cd833bc8395bcc3c8ff251fbd735a389d4f05c
Aaron Froehlich <aaronfroehlich@gmail.com>
2020-04-22 14:57:59 -0500

    Add initial class session entity

---
+++ web/modules/custom/ilr_salesforce/src/ClassSessionInterface.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Drupal\ilr_salesforce\Entity;
+
+use Drupal\Core\Entity\ContentEntityInterface;
+use Drupal\Core\Entity\EntityInterface;
+
+/**
+ * Provides an interface for defining Collection entities.
+ *
+ * @ingroup ilr_salesforce
+ */
+interface ClassSessionInterface extends ContentEntityInterface {
+
+  /**
+   * Add get/set methods for your configuration properties here.
+   */
+
+  /**
+   * Gets the Class session title.
+   *
+   * @return string
+   *   Title of the Class session.
+   */
+  public function getTitle();
+
+  /**
+   * Sets the Class session title.
+   *
+   * @param string $title
+   *   The Class session title.
+   *
+   * @return \Drupal\ilr_salesforce\Entity\ClassSessionInterface
+   *   The called Class session entity.
+   */
+  public function setTitle($title);
+
+}
---
+++ web/modules/custom/ilr_salesforce/src/Entity/ClassSession.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace Drupal\ilr_salesforce\Entity;
+
+use Drupal\Core\Entity\ContentEntityBase;
+use Drupal\Core\Field\BaseFieldDefinition;
+use Drupal\Core\Entity\EntityTypeInterface;
+use Drupal\Core\Entity\ContentEntityInterface;
+use Drupal\datetime\Plugin\Field\FieldType\DateTimeItemInterface;
+
+/**
+ * Defines the Class Session entity.
+ *
+ * @ingroup ilr_salesforce
+ *
+ * @ContentEntityType(
+ *   id = "class_session",
+ *   label = @Translation("Class session"),
+ *   base_table = "class_session",
+ *   entity_keys = {
+ *     "id" = "id",
+ *     "uuid" = "uuid",
+ *     "label" = "title",
+ *   },
+ * )
+ */
+
+class ClassSession extends ContentEntityBase implements ContentEntityInterface {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getTitle() {
+    return $this->get('title')->value;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function setTitle($title) {
+    $this->set('title', $title);
+    return $this;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function baseFieldDefinitions(EntityTypeInterface $entity_type) {
+    $fields = parent::baseFieldDefinitions($entity_type);
+
+    $fields['id'] = BaseFieldDefinition::create('integer')
+      ->setLabel(t('ID'))
+      ->setDescription(t('The ID of the Class Session entity.'))
+      ->setReadOnly(TRUE);
+
+    $fields['uuid'] = BaseFieldDefinition::create('uuid')
+      ->setLabel(t('UUID'))
+      ->setDescription(t('The UUID of the Class Session entity.'))
+      ->setReadOnly(TRUE);
+
+    $fields['title'] = BaseFieldDefinition::create('string')
+      ->setLabel(t('Title'))
+      ->setDescription(t('The title given to this session.'))
+      ->setSettings([
+        'max_length' => 50,
+        'text_processing' => 0,
+      ])
+      ->setDefaultValue('');
+
+    $fields['class'] = BaseFieldDefinition::create('entity_reference')
+      ->setLabel(t('Class'))
+      ->setDescription(t('The class to which this session belongs.'))
+      ->setSetting('target_type', 'node');
+
+    $fields['date'] = BaseFieldDefinition::create('daterange')
+      ->setLabel(t('Date and time'))
+      ->setDescription(t('The start and end date and time.'));
+
+    $fields['address'] = BaseFieldDefinition::create('address')
+      ->setLabel(t('Address'))
+      ->setDescription(t('Address for the session.'));
+
+    return $fields;
+  }
+}
