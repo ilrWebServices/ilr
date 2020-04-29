460ffac63eaf88b65e6a228ea29f96db74ee5cb1
Jeff Amaral <jeff.amaral@cornell.edu>
2020-04-29 16:24:49 -0700

    Add collection listing 'Items' link to operations

--- web/modules/custom/collection/src/CollectionListBuilder.php
+++ web/modules/custom/collection/src/CollectionListBuilder.php
@@ -4,6 +4,7 @@ namespace Drupal\collection;
 
 use Drupal\Core\Entity\EntityInterface;
 use Drupal\Core\Entity\EntityListBuilder;
+use Drupal\Core\Url;
 
 /**
  * Defines a class to build a listing of Collection entities.
@@ -46,6 +47,29 @@ class CollectionListBuilder extends EntityListBuilder {
     $row['name'] = $entity->toLink();
     $row['type'] = $entity->bundle();
     return $row + parent::buildRow($entity);
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function buildOperations(EntityInterface $entity) {
+    $build = parent::buildOperations($entity);
+
+    $collection_items_url = Url::fromRoute(
+      'entity.collection_item.collection',
+      ['collection' => $entity->id()]
+    );
+
+    // Add a link to the collection items listing.
+    $build['#links'] = array_merge([
+      'view_items' => [
+        'title' => $this->t('Items'),
+        'url' => $collection_items_url,
+        'weight' => -1,
+      ]
+    ], $build['#links']);
+
+    return $build;
   }
 
 }
