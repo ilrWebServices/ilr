a5b27c29caf4faf70ee628f6be7135734f07ee94
Jeff Amaral <jeff.amaral@cornell.edu>
2020-05-13 10:18:25 -0700

    Refactor publication branding block
    - Use access control
    - Use publication name as fallback logo
    - Improve caching
    - Remove contexts
    - Code cleanup

--- web/modules/custom/collection_publications/src/Plugin/Block/PublicationBrandingBlock.php
+++ web/modules/custom/collection_publications/src/Plugin/Block/PublicationBrandingBlock.php
@@ -17,90 +17,91 @@ use Drupal\Core\Cache\Cache;
  * @Block(
  *   id = "publication_branding_block",
  *   admin_label = @Translation("Publication branding"),
- *   context_definitions = {
- *     "node" = @ContextDefinition("entity:node", required = FALSE),
- *     "collection" = @ContextDefinition("entity:collection", required = FALSE)
- *   }
  * )
  */
 class PublicationBrandingBlock extends BlockBase implements ContainerFactoryPluginInterface {
 
   /**
-   * Drupal\path_alias_entities\pathAliasEntitieManager definition.
-   *
-   * @var \Drupal\path_alias_entities\pathAliasEntitieManager
+   * An array of entities in the current path.
    */
-  protected $pathAliasEntitieManager;
+  protected $pathAliasEntities;
+
+  /**
+   * The first entity in the path alias, if there is one.
+   */
+  protected $firstPathEntity;
+
+  /**
+   * The last entity in the path alias, if there is one.
+   */
+  protected $lastPathEntity;
 
   /**
    * {@inheritdoc}
    */
   public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
     $instance = new static($configuration, $plugin_id, $plugin_definition);
-    $instance->pathAliasEntitieManager = $container->get('path_alias.entities');
+    $instance->pathAliasEntities = $container->get('path_alias.entities')->getPathAliasEntities();
+    $instance->firstPathEntity = reset($instance->pathAliasEntities);
+    $instance->lastPathEntity = end($instance->pathAliasEntities);
     return $instance;
   }
 
   /**
    * {@inheritdoc}
    */
-  // public function blockAccess(AccountInterface $account) {
-  //   if ($this->getSubsiteFromContext()) {
-  //     return AccessResult::allowed();
-  //   }
+  public function blockAccess(AccountInterface $account) {
+    if (empty($this->pathAliasEntities)) {
+      return AccessResult::neutral();
+    }
 
-  //   return AccessResult::neutral();
-  // }
+    // Only allow access if the first entity in the path alias is a Publication
+    // term.
+    if ($this->firstPathEntity->getEntityType()->id() === 'taxonomy_term' && $this->firstPathEntity->bundle() === 'publication') {
+      return AccessResult::allowed();
+    }
+
+    return AccessResult::neutral();
+  }
 
   /**
    * {@inheritdoc}
    */
   public function build() {
     $build = [];
-    $path_entities = $this->pathAliasEntitieManager->getPathAliasEntities();
+    $build['#cache']['contexts'] = ['url.path'];
+    $cache_tags = $this->firstPathEntity->getCacheTags();
+    $has_svg_logo = !$this->firstPathEntity->field_inline_svg_logo->isEmpty();
 
-    if (empty($path_entities)) {
-      return $build;
+    if ($has_svg_logo) {
+      $build['publication_logo'] = $this->firstPathEntity->field_inline_svg_logo->view('default');
+    }
+    else {
+      $build['publication_logo'] = $this->firstPathEntity->name->view('default');
     }
 
-    $first_path_entity = reset($path_entities);
-
-    // Only continue if the first entity in the path alias is a Publication
-    // term.
-    if ($first_path_entity->getEntityType()->id() !== 'taxonomy_term' || $first_path_entity->bundle() !== 'publication') {
-      return $build;
-    }
-
-    if ($first_path_entity->field_inline_svg_logo->isEmpty()) {
-      return $build;
-    }
-
-    $build['publication_logo'] = $first_path_entity->field_inline_svg_logo->view('default');
     $build['publication_logo']['#attributes']['class'][] = 'publication-logo';
 
-    $last_path_entity = end($path_entities);
+    if ($this->lastPathEntity->getEntityType()->id() === 'collection' && $this->lastPathEntity->bundle() === 'publication_issue') {
+      $cache_tags = array_merge($cache_tags, $this->lastPathEntity->getCacheTags());
 
-    if ($last_path_entity->getEntityType()->id() === 'collection' && $last_path_entity->bundle() === 'publication_issue') {
       $build['publication_issue_title'] = [
-        '#markup' => '<div class="publication-logo--issue">' . $last_path_entity->label() . '</div>',
+        '#markup' => '<div class="publication-logo--issue">' . $this->lastPathEntity->label() . '</div>',
         '#weight' => 10,
       ];
     }
-    elseif ($last_path_entity->getEntityType()->id() === 'node' && $last_path_entity->bundle() === 'story') {
+    elseif ($this->lastPathEntity->getEntityType()->id() === 'node' && $this->lastPathEntity->bundle() === 'story') {
       $build['publication_logo']['#attributes']['class'][] = 'publication-logo--small';
 
-      if (!$first_path_entity->field_subtitle->isEmpty()) {
+      if (!$this->firstPathEntity->field_subtitle->isEmpty()) {
         $build['publication_subtitle'] = [
-          '#markup' => '<div class="publication-logo--subtitle">' . $first_path_entity->field_subtitle->value . '</div>',
+          '#markup' => '<div class="publication-logo--subtitle">' . $this->firstPathEntity->field_subtitle->value . '</div>',
           '#weight' => 10,
         ];
       }
     }
 
-    // dump($build);
-
-    $build['#cache']['max-age'] = 0; // Tmp for testing
-    // dump($build);
+    $build['#cache']['tags'] = $cache_tags;
     return $build;
   }
 
