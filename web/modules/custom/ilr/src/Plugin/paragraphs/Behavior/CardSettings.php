098ca8babaa5261f9dc06e60c61cba3ba388ddd7
Jeff Amaral <jeff.amaral@cornell.edu>
2020-05-29 11:58:09 -0700

    Add a setting to promo (Card) paragraphs to adjust the opacity of the media overlay

---
+++ web/modules/custom/ilr/src/Plugin/paragraphs/Behavior/CardSettings.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace Drupal\ilr\Plugin\paragraphs\Behavior;
+
+use Drupal\paragraphs\ParagraphsBehaviorBase;
+use Drupal\Core\Form\FormStateInterface;
+use Drupal\paragraphs\ParagraphInterface;
+use Drupal\paragraphs\Entity\Paragraph;
+use Drupal\paragraphs\Entity\ParagraphsType;
+use Drupal\Core\Entity\Display\EntityViewDisplayInterface;
+
+/**
+ * Provides a Card paragraph behavior plugin.
+ *
+ * @ParagraphsBehavior(
+ *   id = "ilr_card",
+ *   label = @Translation("Card settings"),
+ *   description = @Translation("Configure Card media opacity and other settings."),
+ *   weight = 3
+ * )
+ */
+class CardSettings extends ParagraphsBehaviorBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function buildBehaviorForm(ParagraphInterface $paragraph, array &$form, FormStateInterface $form_state) {
+    $form['media_overlay_opacity'] = [
+      '#type' => 'range',
+      '#min' => 0,
+      '#max' => 100,
+      '#step' => 10,
+      '#title' => $this->t('Media overlay opacity'),
+      '#default_value' => $paragraph->getBehaviorSetting($this->getPluginId(), 'media_overlay_opacity') ?? '50',
+      '#description' => $this->t('Select a range from transparent to fully opaque.'),
+    ];
+
+    return $form;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function preprocess(&$variables) {
+    $overlay_opacity = $variables['paragraph']->getBehaviorSetting($this->getPluginId(), 'media_overlay_opacity') ?? 50;
+    $variables['attributes']['style'][] = '--cu-overlay-opacity: ' . $overlay_opacity / 100 . ';';
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function view(array &$build, Paragraph $paragraphs_entity, EntityViewDisplayInterface $display, $view_mode) {}
+
+  /**
+   * {@inheritdoc}
+   */
+  public function settingsSummary(Paragraph $paragraph) {
+    $overlay_opacity = $paragraph->getBehaviorSetting($this->getPluginId(), 'media_overlay_opacity') ?? 50;
+
+    $summary = [];
+    $summary[] = [
+      'label' => $this->t('Media overlay opacity'),
+      'value' => $overlay_opacity . '%',
+    ];
+
+    return $summary;
+  }
+
+  /**
+   * {@inheritdoc}
+   *
+   * This behavior is only applicable to promo (Card) paragraphs.
+   */
+  public static function isApplicable(ParagraphsType $paragraphs_type) {
+    return in_array($paragraphs_type->id(), ['promo']);
+  }
+
+}
