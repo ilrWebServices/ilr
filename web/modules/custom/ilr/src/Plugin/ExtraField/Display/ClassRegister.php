f9aef4c2fd9722fed930c9b9ad56ce63af01464d
Jeff Amaral <jeff.amaral@cornell.edu>
2020-06-24 17:16:44 -0600

    Switch the class register block for courses from a custom block to an extra field

--- web/modules/custom/ilr/src/Plugin/ExtraField/Display/ClassRegister.php
+++ web/modules/custom/ilr/src/Plugin/ExtraField/Display/ClassRegister.php
@@ -1,78 +1,47 @@
 <?php
 
-namespace Drupal\ilr\Plugin\Block;
+namespace Drupal\ilr\Plugin\ExtraField\Display;
 
-use Drupal\Core\Block\BlockBase;
+use Drupal\Core\Entity\ContentEntityInterface;
+use Drupal\extra_field\Plugin\ExtraFieldDisplayBase;
 use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
-use Drupal\Core\Entity\EntityTypeManagerInterface;
-
+use Drupal\Core\StringTranslation\StringTranslationTrait;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
- * Provides a 'ClassRegisterBlock' block.
+ * Course Class Register Extra field Display.
  *
- * @deprecated This functionality has been moved to Drupal\ilr\Plugin\ExtraField\Display\ClassRegister.
- *
- * @Block(
- *  id = "class_register_block",
- *  admin_label = @Translation("Class register"),
+ * @ExtraFieldDisplay(
+ *   id = "class_register",
+ *   label = @Translation("Class Register"),
+ *   bundles = {
+ *     "node.course",
+ *   },
+ *   visible = true
  * )
  */
-class ClassRegisterBlock extends BlockBase implements ContainerFactoryPluginInterface {
+class ClassRegister extends ExtraFieldDisplayBase implements ContainerFactoryPluginInterface {
 
-  /**
-   * The entity manager.
-   *
-   * @var \Drupal\Core\Entity\EntityTypeManagerInterface
-   */
-  protected $entityTypeManager;
-
-  /**
-   * Constructs a new ClassRegisterBlock instance.
-   *
-   * @param array $configuration
-   *   A configuration array containing information about the plugin instance.
-   * @param string $plugin_id
-   *   The plugin_id for the plugin instance.
-   * @param mixed $plugin_definition
-   *   The plugin implementation definition.
-   * @param $entity_type_manager
-   *   The entity manager object.
-   */
-  public function __construct(array $configuration, $plugin_id, $plugin_definition, EntityTypeManagerInterface $entity_type_manager) {
-    parent::__construct($configuration, $plugin_id, $plugin_definition);
-    $this->entityTypeManager = $entity_type_manager;
-  }
+  use StringTranslationTrait;
 
   /**
    * {@inheritdoc}
    */
   public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
-    return new static(
-      $configuration,
-      $plugin_id,
-      $plugin_definition,
-      $container->get('entity.manager')
-    );
+    $instance = new static($configuration, $plugin_id, $plugin_definition);
+    $instance->entityTypeManager = $container->get('entity_type.manager');
+    $instance->pathAliasEntitiesManager = $container->get('path_alias.entities');
+    return $instance;
   }
 
   /**
    * {@inheritdoc}
    */
-  public function build() {
+  public function view(ContentEntityInterface $node) {
     $build = [];
-
-    if (!$node = \Drupal::routeMatch()->getParameter('node')) {
-      return $build;
-    }
-
-    if ($node->bundle() != 'course' || !$node->hasField('field_topics')) {
-      return $build;
-    }
-
     $classes = $this->removeOverflowSessions($node->classes->referencedEntities());
-
     $class_info = [];
+
     foreach ($classes as $class) {
       $mapped_objects = $this->entityTypeManager->getStorage('salesforce_mapped_object')
         ->loadByProperties([
@@ -98,7 +67,7 @@ class ClassRegisterBlock extends BlockBase implements ContainerFactoryPluginInte
       ];
     }
 
-    $build = [
+    $build['ilr_class_register_block'] = [
       '#theme' => 'ilr_class_register_block',
       '#classes' => $class_info
     ];
