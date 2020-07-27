89520e6f73832917abf04b8a9f91759a208234ed
Jeff Amaral <jeff.amaral@cornell.edu>
2020-07-27 15:56:57 -0700

    Combine add and update menu graft link methods into a single upsert method
    The hope is that simplifies the code and makes it more readable.

--- web/modules/custom/menu_graft/src/MenuGraftHelper.php
+++ web/modules/custom/menu_graft/src/MenuGraftHelper.php
@@ -39,69 +39,94 @@ class MenuGraftHelper {
   }
 
   /**
-   * Create a 'mirrored' menu entries associated with a new menu_link_content
+   * Update or add a 'mirrored' menu entry associated with a menu_link_content
    * entity.
+   *
+   * Note that this compliments the automatic generation of 'mirrored' graft
+   * menu link items created with the `menu_graft.menu_link` derivative.
+   *
+   * @see \Drupal\menu_graft\Plugin\Derivative\MenuGraftMenuLink
    *
    * @param \Drupal\menu_link_content\MenuLinkContentInterface
    *   $menu_link_content A menu_link_content entity
    */
-  public function addMenuGraftLinkItem(MenuLinkContentInterface $menu_link_content) {
+  public function upsertMenuGraftLinkItem(MenuLinkContentInterface $menu_link_content) {
     $root_menu_info = $this->getGraftRootMenuInfo($menu_link_content->getMenuName());
 
     if (!$root_menu_info) {
       return;
     }
 
-    $plugin_def = $menu_link_content->getPluginDefinition();
+    $root_plugin_def = $menu_link_content->getPluginDefinition();
+    $graft_plugin_id = 'menu_graft.menu_link:' . $root_plugin_def['id'];
+    $graft_def_exists = $this->menuLinkManager->hasDefinition($graft_plugin_id);
 
-    // Create a blank 'mirror' graft link plugin definition.
-    $menu_graft_plugin_def = [
-      // TODO See if there is a way to get class, deriver, and provider automatically.
-      "class" => "Drupal\menu_graft\Plugin\Menu\MenuGraftMenuLink",
-      "deriver" => "Drupal\menu_graft\Plugin\Derivative\MenuGraftMenuLink", // Unnecessary?
-      "provider" => "menu_graft",
-      "menu" => $root_menu_info['menu_name'],
-      "id" => "menu_graft.menu_link:" . $menu_link_content->getPluginId(),
-    ];
+    // Load an existing graft link item definition.
+    if ($graft_def_exists) {
+      $graft_plugin_def = $this->menuLinkManager->getDefinition($graft_plugin_id);
 
-    $menu_graft_plugin_def = $this->mapMenuGraftLinkDefinition($menu_link_content->getPluginDefinition(), $menu_graft_plugin_def, $root_menu_info);
-    $this->menuLinkManager->addDefinition($menu_graft_plugin_def['id'], $menu_graft_plugin_def);
+      // Set a special key that will be detected in
+      // \Drupal\menu_graft\Plugin\Menu\MenuGraftMenuLink::updateLink() so that
+      // normal edit restrictions are ignored.
+      $graft_plugin_def['menu_graft_cascade_update'] = 1;
+    }
+    // Create a new graft link item definition.
+    else {
+      $graft_plugin_def = [
+        // TODO See if there is a way to get class, deriver, and provider automatically.
+        "class" => "Drupal\menu_graft\Plugin\Menu\MenuGraftMenuLink",
+        "deriver" => "Drupal\menu_graft\Plugin\Derivative\MenuGraftMenuLink", // Unnecessary?
+        "provider" => "menu_graft",
+        "menu" => $root_menu_info['menu_name'],
+        "id" => $graft_plugin_id,
+      ];
+    }
+
+    // Update the graft plugin definition with values from the root link plugin
+    // definition.
+    $mapped_keys = ['title', 'description', 'route_name', 'route_parameters', 'weight', 'enabled', 'expanded'];
+    foreach ($mapped_keys as $mapped_key) {
+      $graft_plugin_def[$mapped_key] = $root_plugin_def[$mapped_key];
+    }
+
+    // The parent key is special. If the root menu link parent has a value, the
+    // graft 'mirror' should have its parent set to the 'mirror' copy of the
+    // parent link. Otherwise, it should be at the root of the menu graft point.
+    if ($root_plugin_def['parent']) {
+      $graft_plugin_def['parent'] = 'menu_graft.menu_link:' . $root_plugin_def['parent'];
+    }
+    else {
+      $graft_plugin_def['parent'] = $root_menu_info['menu_link_id'];
+    }
+
+    // Save the changed or new graft item.
+    if ($graft_def_exists) {
+      $this->menuLinkManager->updateDefinition($graft_plugin_def['id'], $graft_plugin_def);
+    }
+    else {
+      $this->menuLinkManager->addDefinition($graft_plugin_def['id'], $graft_plugin_def);
+    }
   }
 
   /**
+   * Remove a 'mirrored' menu entry associated with a menu_link_content entity.
    *
+   * @param \Drupal\menu_link_content\MenuLinkContentInterface
+   *   $menu_link_content A menu_link_content entity
    */
   public function removeMenuGraftLinkItem(MenuLinkContentInterface $menu_link_content) {
-    $menu_graft_plugin_id = 'menu_graft.menu_link:' . $menu_link_content->getPluginId();
-    $this->menuLinkManager->removeDefinition($menu_graft_plugin_id);
+    $graft_plugin_id = 'menu_graft.menu_link:' . $menu_link_content->getPluginId();
+    $this->menuLinkManager->removeDefinition($graft_plugin_id);
   }
 
   /**
+   * Get menu graft settings for a given menu.
    *
-   */
-  public function updateMenuGraftLinkItem(MenuLinkContentInterface $menu_link_content) {
-    $root_menu_info = $this->getGraftRootMenuInfo($menu_link_content->getMenuName());
-
-    if (!$root_menu_info) {
-      return;
-    }
-
-    // Get the 'mirror' graft link plugin definition.
-    $menu_graft_plugin_id = 'menu_graft.menu_link:' . $menu_link_content->getPluginId();
-    $menu_graft_plugin_def = $this->menuLinkManager->getDefinition($menu_graft_plugin_id);
-
-    // Update it.
-    $menu_graft_plugin_def = $this->mapMenuGraftLinkDefinition($menu_link_content->getPluginDefinition(), $menu_graft_plugin_def, $root_menu_info);
-
-    // Set a special key that will be detected in
-    // \Drupal\menu_graft\Plugin\Menu\MenuGraftMenuLink::updateLink() so that
-    // normal edit restrictions are ignored.
-    $menu_graft_plugin_def['menu_graft_cascade_update'] = 1;
-    $this->menuLinkManager->updateDefinition($menu_graft_plugin_def['id'], $menu_graft_plugin_def);
-  }
-
-  /**
+   * @param string $menu_name The name of the menu from which to load the
+   *   settings.
    *
+   * @return array An array with info about the menu name and menu link id onto
+   *   which the menu should be grafted.
    */
   protected function getGraftRootMenuInfo($menu_name) {
     $menu = $this->menuStorage->load($menu_name);
@@ -117,30 +142,6 @@ class MenuGraftHelper {
     else {
       return FALSE;
     }
-  }
-
-  /**
-   *
-   */
-  protected function mapMenuGraftLinkDefinition(array $root_plugin_def, array $graft_plugin_def, array $root_menu_info) {
-    // Update the graft plugin definition with values from the root link plugin
-    // definition.
-    $mapped_keys = ['title', 'description', 'route_name', 'route_parameters', 'weight', 'enabled', 'expanded'];
-    foreach ($mapped_keys as $mapped_key) {
-      $graft_plugin_def[$mapped_key] = $root_plugin_def[$mapped_key];
-    }
-
-    // The parent key is special. If the graft menu link parent has a value,
-    // should have its parent set to the 'mirror' copy of the parent link.
-    // Otherwise, it should be at the root of the menu graft point.
-    if ($root_plugin_def['parent']) {
-      $graft_plugin_def['parent'] = 'menu_graft.menu_link:' . $root_plugin_def['parent'];
-    }
-    else {
-      $graft_plugin_def['parent'] = $root_menu_info['menu_link_id'];
-    }
-
-    return $graft_plugin_def;
   }
 
 }
