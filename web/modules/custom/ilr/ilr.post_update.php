100f8b5cf713fda10511100a75c75fe9957d1d73
Aaron Froehlich <aaronfroehlich@gmail.com>
2020-08-05 15:35:47 -0400

    Update the reference html for the 75th video banner
    This post update hook has already run, but it seems useful to have a reference to the updated html here.

--- web/modules/custom/ilr/ilr.post_update.php
+++ web/modules/custom/ilr/ilr.post_update.php
@@ -508,7 +508,26 @@ function ilr_post_update_create_75_video_block(&$sandbox) {
   $block->info = "75th video banner";
   $content = '<div class="cu-banner--video cu-banner">
   <div class="cu-banner__media">
-    <video autoplay="" class="cu-video" loop="" muted="" src="https://ilr-images.s3.amazonaws.com/video/ilr_hero_clip_75_202008_500.mp4">&nbsp;</video>
+    <div class="video-pause">
+      <div aria-pressed="false"
+        class="cu-icon--inline cu-icon cu-icon--play pause" role="button"><svg
+          class="cu-icon__image" viewBox="0 0 100 100" width="3em"
+          xmlns="http://www.w3.org/2000/svg">
+          <use href="/libraries/union/source/images/icons.svg#pause"></use>
+        </svg>
+        <div class="cu-icon__label sr-only">Pause Video</div>
+      </div>
+      <div aria-pressed="false"
+        class="cu-icon--inline cu-icon cu-icon--play play visually-hidden" role="button">
+        <svg class="cu-icon__image" viewBox="0 0 100 100" width="3em"
+          xmlns="http://www.w3.org/2000/svg">
+          <use href="/libraries/union/source/images/icons.svg#play"></use>
+        </svg>
+        <div class="cu-icon__label sr-only">Play Video</div>
+      </div>
+    </div>
+    <video autoplay="" class="cu-video" loop="" muted=""
+      src="https://ilr-images.s3.amazonaws.com/video/ilr_hero_clip_75_202008_500.mp4">&nbsp;</video>
   </div>
 </div>';
   $block->body->value = $content;
