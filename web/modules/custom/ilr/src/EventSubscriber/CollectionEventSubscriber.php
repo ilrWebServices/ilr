5dcef03612efd385e377253c1a6175e8976fc57c
Aaron Froehlich <aaronfroehlich@gmail.com>
2020-09-30 08:20:07 -0400

    Copy fields and configuration when new blog categories are created

--- web/modules/custom/ilr/src/EventSubscriber/CollectionEventSubscriber.php
+++ web/modules/custom/ilr/src/EventSubscriber/CollectionEventSubscriber.php
@@ -11,6 +11,8 @@ use Symfony\Component\EventDispatcher\Event;
 use Drupal\Core\Messenger\MessengerInterface;
 use Drupal\paragraphs\Entity\Paragraph;
 use Drupal\Core\Url;
+use Drupal\field\Entity\FieldStorageConfig;
+use Drupal\field\Entity\FieldConfig;
 
 /**
  * Class CollectionEventSubscriber.
@@ -89,6 +91,48 @@ class CollectionEventSubscriber implements EventSubscriberInterface {
           '%collection_name' => $collection->label(),
           '@collection_add_url' => $collection_item_add_url->toString(),
         ]));
+      }
+
+      // Load the config for a typical blog category.
+      $category_storage = $this->entityTypeManager->getStorage('taxonomy_vocabulary');
+      $entity_field_manager = \Drupal::service('entity_field.manager');
+      $display_repository = \Drupal::service('entity_display.repository');
+      $field_instance_config = $this->entityTypeManager->getStorage('field_config');
+      $category_form_display = $display_repository->getFormDisplay('taxonomy_term', 'blog_2_categories');
+      $category_view_display = $display_repository->getViewDisplay('taxonomy_term', 'blog_2_categories');
+
+      // Load the vocabulary that was just created and added to this collection.
+      $collection_items = $collection->findItemsByAttribute('blog_taxonomy_categories', TRUE);
+
+      if (empty($collection_items)) {
+        return;
+      }
+
+      $collected_item = reset($collection_items);
+      $new_category = $collected_item->item->entity;
+      $new_category_form_display = $display_repository->getFormDisplay('taxonomy_term', $new_category->id());
+      $new_category_view_display = $display_repository->getViewDisplay('taxonomy_term', $new_category->id());
+
+      foreach ($entity_field_manager->getFieldDefinitions('taxonomy_term', 'blog_2_categories') as $category_field_name => $category_field_def) {
+        // `FieldConfig` definitions are user created fields (i.e. `field_*`).
+        // This filters out base fields, which would be `BaseFieldDefinition`s.
+        if ($category_field_def instanceof FieldConfig) {
+          // Load the field config
+          $base_field_storage_config = FieldStorageConfig::loadByName('taxonomy_term', $category_field_name);
+          // Copy standard field instances to this vocabulary.
+          $new_field_config = $field_instance_config->create([
+            'field_storage' => $base_field_storage_config,
+            'bundle' => $new_category->id(),
+            'label' => $category_field_def->getLabel(),
+            'settings' => $category_field_def->getSettings(),
+          ]);
+          $new_field_config->save();
+
+          // Configure form and view modes to match fields, too.
+          // @todo: fix this to work with layout builder settings.
+          $new_category_form_display->setComponent($category_field_name, $category_form_display->getComponent($category_field_name))->save();
+          $new_category_view_display->setComponent($category_field_name, $category_view_display->getComponent($category_field_name))->save();
+        }
       }
     }
   }
