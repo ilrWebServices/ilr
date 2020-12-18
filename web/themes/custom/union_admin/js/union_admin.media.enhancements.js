e5dbcf0b93dc8033013a8870a3274a30a7204f3b
Jeff Amaral <jeff.amaral@cornell.edu>
2020-12-18 13:11:43 -0500

    Automatically filter media library on duplicate when upload attempted
    Co-authored-by: Aaron Froehlich <aaronfroehlich@gmail.com>

---
+++ web/themes/custom/union_admin/js/union_admin.media.enhancements.js
@@ -0,0 +1,44 @@
+(function ($, Drupal) {
+
+  'use strict';
+
+  Drupal.behaviors.union_admin_media_enhancements = {
+    attach: function (context) {
+      let dupeMessages = $(context).find('.messages--error li:contains("This file has already been uploaded")');
+
+      dupeMessages.each(function(key, value) {
+        let helpText = $(value).text();
+        let mediaName = helpText.match(/"(.*?)"/)[1];
+        let message = $(this);
+
+        if (!mediaName.length) {
+          return false;
+        }
+
+        let mediaLibraryForm = $(document).find('#views-exposed-form-media-library-widget');
+
+        if (!mediaLibraryForm) {
+          return false;
+        }
+
+        let mediaLibraryFilterInput = mediaLibraryForm.find('[data-drupal-selector="edit-name"]');
+
+        $(mediaLibraryFilterInput).val(mediaName);
+        $(mediaLibraryForm).find('.js-form-submit').click();
+
+        // Be helpful and select the duplicate item.
+        setTimeout(function() {
+          let firstDupe = $('.media-library-item').first();
+
+          if (firstDupe) {
+            firstDupe.find('input[type="checkbox"]').attr('checked', 'checked').trigger('change');
+            message.append(Drupal.t(' An existing item from the media library has been selected below.'));
+          }
+        }, 750);
+
+        return false;
+      });
+    }
+  }
+
+})(jQuery, Drupal);
