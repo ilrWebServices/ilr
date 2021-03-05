8a56cafdedda12fc55ddb7767ac88cb819e73996
Jeff Amaral <jeff.amaral@cornell.edu>
2021-03-05 14:08:57 -0500

    Migrate news collection item tags and categories
    This uses a mapping derived from:
    https://docs.google.com/spreadsheets/d/1vID8xyDg05HuGSefeio4QgeHervAFBPyldAxAgwXdls/edit#gid=0
    
    Co-authored-by: Aaron Froehlich <aaronfroehlich@gmail.com>

--- web/modules/custom/ilr_migrate/src/Plugin/migrate/source/IlrD7Node.php
+++ web/modules/custom/ilr_migrate/src/Plugin/migrate/source/IlrD7Node.php
@@ -24,6 +24,35 @@ class IlrD7Node extends d7_node {
       $query->condition('n.status', $this->configuration['node_status']);
     }
 
+    if (isset($this->configuration['node_terms'])) {
+      $term_field = $this->configuration['node_terms'];
+      $query->leftJoin("field_data_{$term_field}", 'fd', 'fd.entity_id = n.nid');
+      $query->leftJoin('taxonomy_term_data', 'td', "td.tid = fd.{$term_field}_tid");
+      $query->addExpression('GROUP_CONCAT(td.name)', 'node_terms');
+
+      // Only return one row per node.
+      $query->groupBy('n.nid');
+
+      // Deal with Drupal's groupBy mysql settings.
+      // @see https://www.drupal.org/project/drupal/issues/567148.
+      $query->groupBy('n.type');
+      $query->groupBy('n.language');
+      $query->groupBy('n.status');
+      $query->groupBy('n.created');
+      $query->groupBy('n.changed');
+      $query->groupBy('n.comment');
+      $query->groupBy('n.promote');
+      $query->groupBy('n.sticky');
+      $query->groupBy('n.tnid');
+      $query->groupBy('n.translate');
+      $query->groupBy('nr.vid');
+      $query->groupBy('nr.title');
+      $query->groupBy('nr.log');
+      $query->groupBy('nr.timestamp');
+      $query->groupBy('n.uid');
+      $query->groupBy('nr.uid');
+    }
+
     return $query;
   }
 
