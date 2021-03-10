7cd53a374e8703809631d0f365ca36f47b7f08fd
Aaron Froehlich <aaronfroehlich@gmail.com>
2021-03-10 17:33:23 -0500

    Add all existing terms associated with collected vocabularies to collections

--- web/modules/custom/collection_blogs/collection_blogs.post_update.php
+++ web/modules/custom/collection_blogs/collection_blogs.post_update.php
@@ -26,3 +26,47 @@ function collection_blogs_post_update_fix_blog_vocabulary_collection_items(&$san
     }
   }
 }
+
+/**
+ * Add all blog terms to their collections.
+ */
+function collection_blogs_post_update_collect_existing_terms(&$sandbox) {
+  $entity_type_manager = \Drupal::service('entity_type.manager');
+  $collection_item_storage = $entity_type_manager->getStorage('collection_item');
+  $term_storage = $entity_type_manager->getStorage('taxonomy_term');
+
+  // Find all auto-generated vocabularies.
+  foreach (['categories', 'tags'] as $vocabulary_type) {
+    $vocabulary_collection_item_ids = $collection_item_storage->getQuery()
+      ->condition('item__target_type', 'taxonomy_vocabulary')
+      ->condition('attributes.key', 'blog_taxonomy_' . $vocabulary_type)
+      ->execute();
+
+    foreach ($collection_item_storage->loadMultiple($vocabulary_collection_item_ids) as $collection_item) {
+      $vocabulary = $collection_item->item->entity;
+      $term_results = $term_storage->getQuery()
+        ->condition('vid', $vocabulary->id())
+        ->execute();
+
+      // Set the default type
+      $collection_type = $collection_item->collection->entity->type->entity;
+      $allowed_collection_item_types = $collection_type->getAllowedCollectionItemTypes('taxonomy_term', $vocabulary->id());
+      $type = ($allowed_collection_item_types)
+        ? reset($allowed_collection_item_types)
+        : 'default';
+
+      foreach ($term_storage->loadMultiple($term_results) as $term) {
+        $collection_item_term = $collection_item_storage->create([
+          'type' => $type,
+          'collection' => $collection_item->collection->entity->id(),
+          'canonical' => TRUE,
+          'changed' => $term->getChangedTime(),
+        ]);
+
+        $collection_item_term->item = $term;
+        $collection_item_term->save();
+      }
+    }
+  }
+}
+
