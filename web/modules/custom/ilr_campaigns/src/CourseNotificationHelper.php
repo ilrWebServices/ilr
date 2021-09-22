173b3795af1a22947f2292c6a88b72b7ea4a2692
Jeff Amaral <jeff.amaral@cornell.edu>
2021-09-22 16:58:08 -0400

    Create and schedule course notification campaigns for new classes
    Co-authored-by: Aaron Froehlich <aaronfroehlich@gmail.com>

--- web/modules/custom/ilr_campaigns/src/CourseNotificationHelper.php
+++ web/modules/custom/ilr_campaigns/src/CourseNotificationHelper.php
@@ -59,6 +59,128 @@ class CourseNotificationHelper {
   }
 
   /**
+   * Send mailing when a new class is created.
+   *
+   * @param \Drupal\Core\Entity\ContentEntityInterface $class
+   *   A new class node for which to trigger a notification email.
+   *
+   * @todo Deal with overflow classes if necessary.
+   *
+   * @return void
+   */
+  public function processNewClass(ContentEntityInterface $class) {
+    $course = $class->field_course->entity;
+    // Bail if this Class and the related Course are not published.
+    if (!$class->isPublished() || !$course->isPublished()) {
+      return;
+    }
+
+    $course_option_name = strtr('!course_name (!course_number)', [
+      '!course_name' => $course->label(),
+      '!course_number' => $course->field_course_number->value,
+    ]);
+
+    $segment_title = $course_option_name . ' Segment';
+
+    // Silently handle exceptions for all REST client API calls.
+    try {
+      // Get the first client_id for this API key. If there are more clients, I
+      // guess we'll need to do something else here.
+      $response = $this->client->get('clients.json');
+      $data = $response->getData();
+      $client_id = $data[0]['ClientID'] ?? FALSE;
+
+      if (empty($client_id)) {
+        return;
+      }
+
+      // Look for a 'Course Notifications' list. Bail if there isn't one.
+      $response = $this->client->get("clients/$client_id/lists.json");
+      $data = $response->getData();
+      $list_array_key = array_search('Course Notifications', array_column($data, 'Name'));
+
+      if ($list_array_key !== FALSE) {
+        $list_id = $data[$list_array_key]['ListID'];
+      }
+      else {
+        // @todo Log missing list.
+        return;
+      }
+
+      // Look up segments for the 'Course Notifications' list for a segment that
+      // has the related course number in the name.
+      $response = $this->client->get("lists/$list_id/segments.json");
+      $segments = $response->getData();
+      $segment_array_key = array_search($segment_title, array_column($segments, 'Title'));
+
+      if ($segment_array_key !== FALSE) {
+        $segment_id = $segments[$segment_array_key]['SegmentID'];
+      }
+      // If there is no segment, create one for this course called 'COURSE_NAME
+      // (COURSE_NUMBER) Segment', e.g. Advanced Collective Bargaining (LS252)
+      // Segment.
+      //   - Single rule is 'Course notifications' matches exactly COURSE_NAME
+      //     (COURSE_NUMBER)
+      else {
+        $data = [
+          'json' => [
+            'Title' => $segment_title,
+            'RuleGroups' => [
+              [
+                'Rules' => [
+                  [
+                    'RuleType' => '[CourseNotifications]',
+                    'Clause' => 'EQUALS ' . $course_option_name,
+                  ],
+                ],
+              ],
+            ],
+          ],
+        ];
+
+        $response = $this->client->post("segments/$list_id.json", $data);
+        $segment_id = $response->getData();
+      }
+
+      // Create a new campaign for this Course and Class, using the class html
+      // page.
+      $data = [
+        'json' => [
+          'Name' => 'Course notification for ' . $course->field_course_number->value,
+          'Subject' => 'New date announced for ' . $course->label(),
+          'FromName' => 'ILR Customer Service',
+          'FromEmail' => 'ilrcustomerservice@cornell.edu',
+          'ReplyTo' => 'ilrcustomerservice@cornell.edu',
+          'HtmlUrl' => $class->toUrl('ilr_campaigns_email', ['absolute' => TRUE])->toString(),
+          'SegmentIDs' => [$segment_id],
+        ],
+      ];
+
+      $response = $this->client->post("campaigns/$client_id.json", $data);
+      $campaign_id = $response->getData();
+
+      // Set to send at 9am next day.
+      $send_date = new \DateTime('tomorrow');
+      $send_date->setTime(9, 01);
+
+      $data = [
+        'json' => [
+          'ConfirmationEmail' => 'ilrweb@cornell.edu',
+          'SendDate' => $send_date->format('Y-m-d H:i'),
+        ],
+      ];
+
+      $response = $this->client->post("campaigns/$campaign_id/send.json", $data);
+
+      // Log the details of the new campaign with much detail.
+    }
+    catch (\Exception $e) {
+      // @todo Log and continue. No WSOD for us!
+      throw new \Exception($e->getMessage());
+    }
+  }
+
+  /**
    * Add course options to the 'Course Notifications' custom field.
    *
    * @todo Consider NOT removing/replacing renamed options, since that will remove users from segments.
