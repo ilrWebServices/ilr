5d082a20a2e883fe9fcabd50dd95b8c9b7b3db06
Jeff Amaral <jeff.amaral@cornell.edu>
2022-02-16 12:56:46 -0500

    Update layout of persona add/edit form
    Co-authored-by: Aaron Froehlich <aaronfroehlich@gmail.com>

--- web/modules/custom/person/src/Form/PersonaForm.php
+++ web/modules/custom/person/src/Form/PersonaForm.php
@@ -2,8 +2,13 @@
 
 namespace Drupal\person\Form;
 
+use Drupal\Component\Datetime\TimeInterface;
+use Drupal\Core\Datetime\DateFormatterInterface;
 use Drupal\Core\Entity\ContentEntityForm;
+use Drupal\Core\Entity\EntityRepositoryInterface;
+use Drupal\Core\Entity\EntityTypeBundleInfoInterface;
 use Drupal\Core\Form\FormStateInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
  * Form controller for Persona add/edit forms.
@@ -13,10 +18,48 @@ use Drupal\Core\Form\FormStateInterface;
 class PersonaForm extends ContentEntityForm {
 
   /**
+   * The date formatter service.
+   *
+   * @var \Drupal\Core\Datetime\DateFormatterInterface
+   */
+  protected $dateFormatter;
+
+  /**
+   * Constructs a NodeForm object.
+   *
+   * @param \Drupal\Core\Entity\EntityRepositoryInterface $entity_repository
+   *   The entity repository.
+   * @param \Drupal\Core\Entity\EntityTypeBundleInfoInterface $entity_type_bundle_info
+   *   The entity type bundle service.
+   * @param \Drupal\Component\Datetime\TimeInterface $time
+   *   The time service.
+   * @param \Drupal\Core\Datetime\DateFormatterInterface $date_formatter
+   *   The date formatter service.
+   */
+  public function __construct(EntityRepositoryInterface $entity_repository, EntityTypeBundleInfoInterface $entity_type_bundle_info = NULL, TimeInterface $time = NULL, DateFormatterInterface $date_formatter) {
+    parent::__construct($entity_repository, $entity_type_bundle_info, $time);
+    $this->dateFormatter = $date_formatter;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container) {
+    return new static(
+      $container->get('entity.repository'),
+      $container->get('entity_type.bundle.info'),
+      $container->get('datetime.time'),
+      $container->get('date.formatter')
+    );
+  }
+
+  /**
    * {@inheritdoc}
    */
   public function buildForm(array $form, FormStateInterface $form_state) {
     $form = parent::buildForm($form, $form_state);
+
+    /** @var \Drupal\person\Entity\Persona $persona */
     $persona = $this->entity;
 
     if (isset($form['person']) && isset($persona->person->entity)) {
@@ -55,6 +98,42 @@ class PersonaForm extends ContentEntityForm {
     }
 
     $form['revision']['#default_value'] = TRUE;
+
+    // See person_theme().
+    $form['#theme'] = ['persona_edit_form'];
+
+    // Advanced is created in the parent class/
+    $form['advanced']['#type'] = 'container';
+    $form['advanced']['#attributes']['class'][] = 'entity-meta';
+    $form['revision_information']['#type'] = 'container';
+
+    // Similar to NodeForm.
+    $form['meta'] = [
+      '#type' => 'container',
+      '#group' => 'advanced',
+      '#weight' => -10,
+      '#attributes' => ['class' => ['entity-meta__header']],
+      '#tree' => TRUE,
+    ];
+    $form['meta']['changed'] = [
+      '#type' => 'item',
+      '#title' => $this->t('Last saved'),
+      '#markup' => !$persona->isNew() ? $this->dateFormatter->format($persona->getChangedTime(), 'short') : $this->t('Not saved yet'),
+      '#wrapper_attributes' => ['class' => ['entity-meta__last-saved']],
+      '#weight' => 100,
+    ];
+
+    // `person` and `admin_label` are base fields, while revision_info was added
+    // in the parent class.
+    $form['person']['#group'] = 'meta';
+    $form['admin_label']['#group'] = 'meta';
+    $form['revision_information']['#group'] = 'meta';
+
+    // Place the status/published field at the bottom of the form.
+    $form['status']['#group'] = 'footer';
+
+    // Include CSS library for layout.
+    $form['#attached']['library'][] = 'person/persona-form';
 
     return $form;
   }
