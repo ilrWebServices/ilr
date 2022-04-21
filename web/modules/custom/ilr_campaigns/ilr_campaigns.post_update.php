1c8e19bd6308e66b3758b15a2e58c79ac772f6be
Jeff Amaral <jeff.amaral@cornell.edu>
2022-04-21 17:48:25 -0400

    Move email update submissions to blog_updates webform

--- web/modules/custom/ilr_campaigns/ilr_campaigns.post_update.php
+++ web/modules/custom/ilr_campaigns/ilr_campaigns.post_update.php
@@ -14,3 +14,54 @@ function ilr_campaigns_post_update_state_fix() {
   \Drupal::state()->delete('ilr_campaigns.course_notifier_subscriber_last_serial');
   \Drupal::state()->delete('ilr_campaigns.custom_field_update');
 }
+
+/**
+ * Move email update submissions to new blog updates webform.
+ */
+function ilr_campaigns_post_update_move_email_update_subscribers(&$sandbox) {
+  $submission_storage = \Drupal::entityTypeManager()->getStorage('webform_submission');
+
+  $query = $submission_storage->getQuery()
+    ->accessCheck(FALSE)
+    ->condition('webform_id', ['work_and_coronavirus_updates', '75th_updates', 'news_updates'], 'IN')
+    ->exists('entity_id')
+    ->sort('created');
+
+  if (!isset($sandbox['total'])) {
+    $total_query = clone $query;
+    $sandbox['total'] = $total_query->count()->execute();
+    $sandbox['current'] = 0;
+  }
+
+  // Get all legacy email update form submissions.
+  $submission_ids = $query->range($sandbox['current'], 25)->execute();
+
+  // Add them to the blog updates form as new submissions.
+  /** @var \Drupal\webform\WebformSubmissionInterface $submission */
+  foreach ($submission_storage->loadMultiple($submission_ids) as $submission) {
+    /** @var \Drupal\webform\WebformSubmissionInterface $new_submission */
+    $new_submission = $submission_storage->create([
+      'webform_id' => 'blog_updates',
+      'uri' => $submission->uri->value,
+      'created' => $submission->created->value,
+      'completed' => $submission->completed->value,
+      'changed' => $submission->changed->value,
+      'remote_addr' => $submission->remote_addr->value,
+      'entity_type' => $submission->entity_type->value,
+      'entity_id' => $submission->entity_id->value,
+    ]);
+
+    $new_submission->setData($submission->getData());
+    $new_submission->save();
+    $sandbox['current']++;
+  }
+
+  \Drupal::messenger()->addMessage($sandbox['current'] . ' submissions processed.');
+
+  if ($sandbox['current'] >= $sandbox['total']) {
+    $sandbox['#finished'] = 1;
+  }
+  else {
+    $sandbox['#finished'] = ($sandbox['current'] / $sandbox['total']);
+  }
+}
