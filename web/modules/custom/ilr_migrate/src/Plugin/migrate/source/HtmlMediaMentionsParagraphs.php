a224926bf00f719bfebcd9878a9f14fcbe957387
Jeff Amaral <jeff.amaral@cornell.edu>
2022-05-12 17:30:51 -0400

    WIP: Include the year on the dates

--- web/modules/custom/ilr_migrate/src/Plugin/migrate/source/HtmlMediaMentionsParagraphs.php
+++ web/modules/custom/ilr_migrate/src/Plugin/migrate/source/HtmlMediaMentionsParagraphs.php
@@ -18,7 +18,10 @@ use Drupal\migrate\Plugin\migrate\source\SqlBase;
 class HtmlMediaMentionsParagraphs extends SqlBase {
 
   // https://regex101.com/r/HF9dxl/1
-  protected $regex = '/(?<source>.*)\s+[-–]\s+(?<date>.*)\s+(?:(?:[-–]\s+)|(?:[-–]&nbsp;))(?<expert>.*)<.*\n.*(?<url>https?:\/\/.*)".*>(?<title>.*)</mU';
+  protected $itemRegex = '/(?<source>.*)\s+[-–]\s+(?<date>.*)\s+(?:(?:[-–]\s+)|(?:[-–]&nbsp;))(?<expert>.*)<.*\n.*(?<url>https?:\/\/.*)".*>(?<title>.*)</mU';
+
+  // https://regex101.com/r/qu8ym0/1
+  protected $monthSplitRegex = '/<h\d>(?:.|\n)*\s+(?<year>\d{4})<\/h\d>/mU';
 
   /**
    * {@inheritdoc}
@@ -46,7 +49,7 @@ class HtmlMediaMentionsParagraphs extends SqlBase {
     $count = 0;
 
     foreach ($this->query()->execute() as $row) {
-      preg_match_all($this->regex, $row['field_body_value'], $matches, PREG_SET_ORDER);
+      preg_match_all($this->itemRegex, $row['field_body_value'], $matches, PREG_SET_ORDER);
       $count += count($matches);
     }
 
@@ -63,18 +66,39 @@ class HtmlMediaMentionsParagraphs extends SqlBase {
     $all_matches = [];
 
     foreach ($this->query->execute() as $row) {
-      preg_match_all($this->regex, $row['field_body_value'], $matches, PREG_SET_ORDER);
+      // Split each body field into chunks via the <h3>MONTH YEAR</h3> string.
+      $month_chunks = preg_split($this->monthSplitRegex, $row['field_body_value'], 0, PREG_SPLIT_NO_EMPTY|PREG_SPLIT_DELIM_CAPTURE);
+      $year = '';
 
-      foreach ($matches as $key => $match) {
-        // Remove the numeric keys from each match and overwrite it.
-        $matches[$key] = array_filter($match, 'is_string', ARRAY_FILTER_USE_KEY);
+      // print_r($month_chunks);
 
-        // Add a unique id so that migrate can create a mapping.
-        $matches[$key]['id'] = sha1(serialize($match));
+      foreach ($month_chunks as $month_chunk) {
+        // Sometimes month is just a four digit year, because of the
+        // PREG_SPLIT_DELIM_CAPTURE flag and the <year> named group on
+        // preg_split() above. That'll be the year for this chunk of mentions.
+        if (preg_match('/^\d{4}$/', $month_chunk)) {
+          $year = $month_chunk;
+        }
+
+        preg_match_all($this->itemRegex, $month_chunk, $matches, PREG_SET_ORDER);
+
+        foreach ($matches as $key => $match) {
+          // Remove the numeric keys from each match and overwrite it.
+          $matches[$key] = array_filter($match, 'is_string', ARRAY_FILTER_USE_KEY);
+
+          // Append the year to the date field. Otherwise, the date would just
+          // be a month and day.
+          $matches[$key]['date'] .= ' ' . $year;
+
+          // Add a unique id so that migrate can create a mapping.
+          $matches[$key]['id'] = sha1(serialize($match));
+        }
+
+        $all_matches = array_merge($all_matches, $matches);
       }
-
-      $all_matches = array_merge($all_matches, $matches);
     }
+
+    print_r($all_matches); die();
 
     return new \ArrayIterator($all_matches);
     // $statement = $this->query->execute();
