d0c83708fa941c8aeddd76199c2ffcddfdab7b72
Jeff Amaral <jeff.amaral@cornell.edu>
2022-06-29 10:25:24 -0400

    Fix issue with unique media mention url validation and existing duplicates
    Co-authored-by: Aaron Froehlich <aaronfroehlich@gmail.com>

--- web/modules/custom/ilr/src/Plugin/Validation/Constraint/UniqueMediaMentionExternalLinkValidator.php
+++ web/modules/custom/ilr/src/Plugin/Validation/Constraint/UniqueMediaMentionExternalLinkValidator.php
@@ -24,11 +24,23 @@ class UniqueMediaMentionExternalLinkValidator extends ConstraintValidator {
       return;
     }
 
+    $entity = $field_external_link->getEntity();
+
+    // If editing existing entity and there's no change to the field, skip
+    // validation.
+    if (!$entity->isNew()) {
+      $original_entity = \Drupal::entityTypeManager()->getStorage($entity->getEntityTypeId())->loadUnchanged($entity->id());
+
+      if ($field_external_link->uri === $original_entity->field_external_link->uri) {
+        return;
+      }
+    }
+
     $query = \Drupal::entityTypeManager()->getStorage('node')->getQuery();
     $query->condition('field_external_link', $field_external_link->uri);
 
     // Don't include self when updating a media mention.
-    if ($nid = $field_external_link->getEntity()->id()) {
+    if ($nid = $entity->id()) {
       $query->condition('nid', $nid, '!=');
     }
 
