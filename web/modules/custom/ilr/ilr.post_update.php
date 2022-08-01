2706e3f6793a84fd9688f95a9c0cfaaf7664de01
Jeff Amaral <jeff.amaral@cornell.edu>
2022-08-01 12:03:22 -0400

    Update social media icons in footer
    Co-authored-by: Aaron Froehlich <aaronfroehlich@gmail.com>

--- web/modules/custom/ilr/ilr.post_update.php
+++ web/modules/custom/ilr/ilr.post_update.php
@@ -1018,3 +1018,30 @@ function ilr_post_update_fix_rich_text_icons(&$sandbox) {
     $paragraph->save();
   }
 }
+
+/**
+ * Update social footer block.
+ */
+function ilr_post_update_update_social_footer_block_icons(&$sandbox) {
+  $block = \Drupal::service('entity.repository')->loadEntityByUuid('block_content', '48bd16f4-0fe8-4b1a-800b-089c03c0be23');
+  $block->body->value = '<div class="social-follow">
+    <ul class="social-follow__items">
+      <li class="social-follow__item"><a class="social-follow__link" href="https://www.linkedin.com/company/cornell-university-ilr-school"><svg class="cu-icon__image" viewbox="0 0 100 100" width="1.8em" xmlns="http://www.w3.org/2000/svg">
+      <title></title>
+      <use href="/sites/default/files-d8/union/images/icons.svg#linkedin"></use></svg></a></li>
+      <li class="social-follow__item"><a class="social-follow__link" href="https://facebook.com/ilrschool"><svg class="cu-icon__image" viewbox="0 0 100 100" width="1.4em" xmlns="http://www.w3.org/2000/svg">
+      <title></title>
+      <use href="/sites/default/files-d8/union/images/icons.svg#facebook"></use></svg></a></li>
+      <li class="social-follow__item"><a class="social-follow__link" href="https://twitter.com/CornellILR"><svg class="cu-icon__image" viewbox="0 0 100 100" width="2em" xmlns="http://www.w3.org/2000/svg">
+      <title></title>
+      <use href="/sites/default/files-d8/union/images/icons.svg#twitter"></use></svg></a></li>
+      <li class="social-follow__item"><a class="social-follow__link" href="https://instagram.com/cornellilr"><svg class="cu-icon__image" viewbox="0 0 100 100" width="1em" xmlns="http://www.w3.org/2000/svg">
+      <title></title>
+      <use href="/sites/default/files-d8/union/images/icons.svg#instagram"></use></svg></a></li>
+      <li class="social-follow__item"><a class="social-follow__link" href="https://www.youtube.com/user/CornellUniversityILR?sub_confirmation=1"><svg class="cu-icon__image" viewbox="0 0 100 100" width="1.5em" xmlns="http://www.w3.org/2000/svg">
+      <title></title>
+      <use href="/sites/default/files-d8/union/images/icons.svg#youtube"></use></svg></a></li>
+    </ul>
+  </div>';
+  $block->save();
+}
