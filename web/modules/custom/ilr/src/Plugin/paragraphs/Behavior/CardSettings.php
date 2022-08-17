3f2f5225aff5c8d475fc3046a9136e319338b907
Jeff Amaral <jeff.amaral@cornell.edu>
2022-08-17 12:57:43 -0400

    Only display overlay button variant in promo cards with media or vibrant background
    Co-authored-by: Aaron Froehlich <aaronfroehlich@gmail.com>

--- web/modules/custom/ilr/src/Plugin/paragraphs/Behavior/CardSettings.php
+++ web/modules/custom/ilr/src/Plugin/paragraphs/Behavior/CardSettings.php
@@ -256,7 +256,10 @@ class CardSettings extends ParagraphsBehaviorBase {
 
       // Set the button class attribute for legacy promos.
       $variables['button_attributes'] = new Attribute();
-      $variables['button_attributes']->setAttribute('class', ['cu-button--overlay']);
+
+      if ($has_media || $bg_color === 'vibrant') {
+        $variables['button_attributes']->setAttribute('class', ['cu-button--overlay']);
+      }
 
       if ($has_media) {
         $variables['attributes']['class'][] = 'cu-card--promo-with-media';
