c4f408b99d3c111e83f62858e9182161f36b7538
Jeff Amaral <jeff.amaral@cornell.edu>
2023-01-10 17:09:08 -0500

    Log webform email confirmations as notes on the submission

--- web/modules/custom/webform_email_confirm/src/controller/EmailConfirm.php
+++ web/modules/custom/webform_email_confirm/src/controller/EmailConfirm.php
@@ -3,9 +3,11 @@
 namespace Drupal\webform_email_confirm\Controller;
 
 use Drupal\Core\Controller\ControllerBase;
+use Drupal\Core\Datetime\DateFormatterInterface;
 use Drupal\Core\TempStore\SharedTempStore;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Request;
 
 /**
  * Controller for webform email confirm route(s).
@@ -24,9 +26,12 @@ class EmailConfirm extends ControllerBase {
    *
    * @param \Drupal\Core\TempStore\SharedTempStore $tempstore
    *   The shared tempstore.
+   * @param \Drupal\Core\Datetime\DateFormatterInterface $date_formatter
+   *   The date formatter.
    */
-  public function __construct(SharedTempStore $tempstore) {
+  public function __construct(SharedTempStore $tempstore, DateFormatterInterface $date_formatter) {
     $this->tempstore = $tempstore;
+    $this->dateFormatter = $date_formatter;
   }
 
   /**
@@ -34,7 +39,8 @@ class EmailConfirm extends ControllerBase {
    */
   public static function create(ContainerInterface $container) {
     return new static(
-      $container->get('tempstore.shared')->get('webform_email_confirm')
+      $container->get('tempstore.shared')->get('webform_email_confirm'),
+      $container->get('date.formatter')
     );
   }
 
@@ -46,7 +52,7 @@ class EmailConfirm extends ControllerBase {
    * can be an exception to the rule. See the discussion at
    * https://stackoverflow.com/q/1066611
    */
-  public function confirm($token) {
+  public function confirm($token, Request $request) {
     $confirmation_data = $this->tempstore->get('submission_confirmation_' . $token);
 
     if ($confirmation_data->sid) {
@@ -58,6 +64,16 @@ class EmailConfirm extends ControllerBase {
       if (isset($confirmation_status) && $confirmation_status !== 'confirmed') {
         $data[$confirmation_data->confirmation_status_element] = 'confirmed';
         $webform_submission->setData($data);
+
+        // Log the confirmation as a note on the submission.
+        $request_time = $request->server->get('REQUEST_TIME');
+        $note = strtr('Email confirmation: @email at @datetime', [
+          '@email' => $confirmation_data->email,
+          '@datetime' => $this->dateFormatter->format($request_time, 'custom', 'Y-m-d h:i:s'),
+        ]);
+        $notes = $webform_submission->getNotes();
+        $notes .= ($notes ? PHP_EOL : '') . $note;
+        $webform_submission->setNotes($notes);
 
         if ($webform_submission->save()) {
           $this->tempstore->delete('submission_confirmation_' . $token);
