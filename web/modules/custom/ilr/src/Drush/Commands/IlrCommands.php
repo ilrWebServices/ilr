f497ad69fa909cc33a217b556be03b79e6f93c18
Jeff Amaral <jeff.amaral@cornell.edu>
2024-01-08 17:28:28 -0500

    Update custom commands for drush 12

--- web/modules/custom/ilr/src/Drush/Commands/IlrCommands.php
+++ web/modules/custom/ilr/src/Drush/Commands/IlrCommands.php
@@ -1,36 +1,47 @@
 <?php
 
-namespace Drupal\ilr\Commands;
+namespace Drupal\ilr\Drush\Commands;
 
-use Drush\Commands\DrushCommands;
-use Drush\Drush;
 use Drupal\Core\Entity\EntityInterface;
-use Drupal\paragraphs\Entity\Paragraph;
+use Drupal\Core\Entity\EntityTypeManagerInterface;
 use Drupal\paragraphs\ParagraphInterface;
+use Drush\Attributes as CLI;
+use Drush\Commands\DrushCommands;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
- * A Drush commandfile to load the host entity for a paragraph.
+ * A Drush commandfile for ILR custom drush commands.
  */
-class ParagraphsHostEntity extends DrushCommands {
+final class IlrCommands extends DrushCommands {
+
+  /**
+   * Constructs an IlrCommands object.
+   */
+  public function __construct(
+    private readonly EntityTypeManagerInterface $entityTypeManager,
+  ) {
+    parent::__construct();
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container) {
+    return new static(
+      $container->get('entity_type.manager'),
+    );
+  }
 
   /**
    * Load the 'host entity' and output its url. This is useful for 'nested' paragraphs.
-   *
-   * @param string $pid
-   *   The paragaphs item id.
-   *
-   * @command paragraphs:host
-   * @aliases phe,paragraphs-host
-   * @option pid The paragraphs item id.
-   * @usage paragraphs:host --pid=1234
-   *   Output the url for the host entity for paragraph 1234.
    */
-  public function host($pid, $options = ['go' => FALSE, 'browser' => TRUE]) {
-    if (!Drush::bootstrapManager()->doBootstrap(DRUSH_BOOTSTRAP_DRUPAL_FULL)) {
-      throw new \Exception(dt('Unable to bootstrap Drupal.'));
-    }
+  #[CLI\Command(name: 'ilr:paragraphs-host', aliases: ['ph', 'phe'])]
+  #[CLI\Argument(name: 'pid', description: 'The paragraphs item id.')]
+  #[CLI\Usage(name: 'ilr:paragraphs-host 1234', description: 'Output the url for the host entity for paragraph 1234')]
+  public function pheCommand($pid, $options = []) {
+    $paragraph_storage = $this->entityTypeManager->getStorage('paragraph');
 
-    if (!$paragraph = Paragraph::load($pid)) {
+    if (!$paragraph = $paragraph_storage->load($pid)) {
       throw new \Exception(dt('Unable to load paragraph by id: !pid', ['!pid' => $pid]));
     }
 
@@ -60,4 +71,5 @@ class ParagraphsHostEntity extends DrushCommands {
     }
     return $entity;
   }
+
 }
