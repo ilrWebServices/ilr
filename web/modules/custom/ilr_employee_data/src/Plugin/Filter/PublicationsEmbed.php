ba937354dceeb81e2562b21a71a33e8681de3c6d
Jeff Amaral <jeff.amaral@cornell.edu>
2024-09-05 15:28:09 -0400

    Add and enable ilr_employee_data module
    This module can fetch data about employees, such as
    faculty and academic publications, from services like
    Activity Insight.
    
    Currently exploring the idea of using a custom tag in text formats called <ilr-publications> that has a netid attribute.
    
    Other remote data about employees would go in this module.
    
    Co-authored-by: Aaron Froehlich <aaronfroehlich@gmail.com>

---
+++ web/modules/custom/ilr_employee_data/src/Plugin/Filter/PublicationsEmbed.php
@@ -0,0 +1,183 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Drupal\ilr_employee_data\Plugin\Filter;
+
+use Drupal\Component\Utility\Html;
+use Drupal\Core\Logger\LoggerChannelFactoryInterface;
+use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
+use Drupal\Core\Render\BubbleableMetadata;
+use Drupal\Core\Render\RenderContext;
+use Drupal\Core\Render\RendererInterface;
+use Drupal\filter\FilterProcessResult;
+use Drupal\filter\Plugin\FilterBase;
+use Drupal\filter\Plugin\FilterInterface;
+use Drupal\filter\Attribute\Filter;
+use Drupal\Core\StringTranslation\TranslatableMarkup;
+use Drupal\ilr_employee_data\PublicationsFetcher;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use Spatie\SchemaOrg\BaseType;
+
+/**
+ * Provides a filter to embed publication items using a custom tag.
+ *
+ * <ilr-publications data-netid="foo123" data-citation-format="mla" />
+ *
+ * @internal
+ */
+#[Filter(
+  id: "ilr_publications_embed",
+  title: new TranslatableMarkup("Embed publications"),
+  description: new TranslatableMarkup("Embeds publication items using a custom tag, <code>&lt;ilr-publications&gt;</code>."),
+  type: FilterInterface::TYPE_TRANSFORM_REVERSIBLE,
+  weight: 100,
+  settings: [
+    "allowed_citation_formats" => [],
+  ],
+)]
+class PublicationsEmbed extends FilterBase implements ContainerFactoryPluginInterface {
+
+  public function __construct(
+    array $configuration,
+    $plugin_id,
+    $plugin_definition,
+    protected PublicationsFetcher $publicationsFetcher,
+    protected RendererInterface $renderer,
+    protected LoggerChannelFactoryInterface $loggerFactory
+  ) {
+    parent::__construct($configuration, $plugin_id, $plugin_definition);
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
+    return new static(
+      $configuration,
+      $plugin_id,
+      $plugin_definition,
+      $container->get('ilr_employee_data.publications'),
+      $container->get('renderer'),
+      $container->get('logger.factory')
+    );
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function process($text, $langcode): FilterProcessResult {
+    $result = new FilterProcessResult($text);
+
+    if (stristr($text, '<ilr-publications') === FALSE) {
+      return $result;
+    }
+
+    $dom = Html::load($text);
+    $xpath = new \DOMXPath($dom);
+
+    foreach ($xpath->query('//ilr-publications[normalize-space(@data-netid)!=""]') as $node) {
+      /** @var \DOMElement $node */
+      $netid = $node->getAttribute('data-netid');
+      $citation_format = $node->getAttribute('data-citation-format') ?: 'mla';
+      $publications_data = $this->publicationsFetcher->getPublications($netid);
+      $build = [];
+
+      if (empty($publications_data)) {
+        $this->loggerFactory->get('ilr_employee_data')->info('No publications data found for: %netid.', [
+          '%netid' => $netid,
+        ]);
+
+        continue;
+      }
+
+      foreach ($publications_data as $pubgroup => $items) {
+        $clean_pubgroup = HTML::cleanCssIdentifier($pubgroup);
+
+        $build[$clean_pubgroup] = [
+          '#theme' => 'item_list',
+          '#title' => $pubgroup,
+          '#items' => [],
+        ];
+
+        /** @var BaseType $item */
+        foreach ($items as $item) {
+          $build[$clean_pubgroup]['#items'][] = [
+            '#type' => 'component',
+            '#component' => 'ilr_employee_data:' . strtolower($item->getType()),
+            '#props' => $item->getProperties(),
+          ];
+        }
+      }
+
+      $this->renderIntoDomNode($build, $node, $result);
+    }
+
+    $result->setProcessedText(Html::serialize($dom));
+    return $result;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function tips($long = FALSE): string {
+    return (string) $this->t('@todo Provide filter tips here.');
+  }
+
+  /**
+   * Renders the given render array into the given DOM node.
+   *
+   * @param array $build
+   *   The render array to render in isolation.
+   * @param \DOMNode $node
+   *   The DOM node to render into.
+   * @param \Drupal\filter\FilterProcessResult $result
+   *   The accumulated result of filter processing, updated with the metadata
+   *   bubbled during rendering.
+   */
+  protected function renderIntoDomNode(array $build, \DOMNode $node, FilterProcessResult &$result) {
+    // We need to render the embedded publications list:
+    // - without replacing placeholders, so that the placeholders are
+    //   only replaced at the last possible moment. Hence we cannot use
+    //   either renderInIsolation() or renderRoot(), so we must use render().
+    // - without bubbling beyond this filter, because filters must
+    //   ensure that the bubbleable metadata for the changes they make
+    //   when filtering text makes it onto the FilterProcessResult
+    //   object that they return ($result). To prevent that bubbling, we
+    //   must wrap the call to render() in a render context.
+    $markup = $this->renderer->executeInRenderContext(new RenderContext(), function () use (&$build) {
+      return $this->renderer->render($build);
+    });
+    $result = $result->merge(BubbleableMetadata::createFromRenderArray($build));
+    static::replaceNodeContent($node, (string) $markup);
+  }
+
+  /**
+   * Replaces the contents of a DOMNode.
+   *
+   * @param \DOMNode $node
+   *   A DOMNode object.
+   * @param string $content
+   *   The text or HTML that will replace the contents of $node.
+   */
+  protected static function replaceNodeContent(\DOMNode &$node, $content) {
+    if (strlen($content)) {
+      // Load the content into a new DOMDocument and retrieve the DOM nodes.
+      $replacement_nodes = Html::load($content)->getElementsByTagName('body')
+        ->item(0)
+        ->childNodes;
+    }
+    else {
+      $replacement_nodes = [$node->ownerDocument->createTextNode('')];
+    }
+
+    foreach ($replacement_nodes as $replacement_node) {
+      // Import the replacement node from the new DOMDocument into the original
+      // one, importing also the child nodes of the replacement node.
+      $replacement_node = $node->ownerDocument->importNode($replacement_node, TRUE);
+      $node->parentNode->insertBefore($replacement_node, $node);
+    }
+    $node->parentNode->removeChild($node);
+  }
+
+}
---
+++ web/modules/custom/ilr_employee_data/src/PublicationsFetcher.php
@@ -0,0 +1,172 @@
+<?php
+
+namespace Drupal\ilr_employee_data;
+
+use GuzzleHttp\Client;
+use SimpleXMLElement;
+use Spatie\SchemaOrg\Schema;
+
+/**
+ * Fetches remote publications data from external services.
+ */
+class PublicationsFetcher {
+
+  public function __construct(
+    protected Client $client
+  ) {}
+
+  /**
+   * Get publications for a given person via their netid.
+   *
+   * @param string $netid
+   * @param boolean $published_only
+   * @param boolean $public_only
+   * @return array
+   *   An array of schema.org objects keyed by publication categories (e.g.
+   *   News Articles, Books).
+   */
+  public function getPublications(string $netid, bool $published_only = TRUE, bool $public_only = TRUE): array {
+    // Do basic validation of NetID.
+    if (preg_match('/^[a-z]{2,4}\d{1,5}$/', $netid) === FALSE) {
+      // @todo throw error to prevent stuff?
+      return [];
+    }
+
+    return $this->getActivityInsightPublications($netid, $published_only, $public_only);
+  }
+
+  /**
+   * Get publications from Activity Insight for a given netid.
+   *
+   * @param string $netid
+   * @param boolean $published_only
+   * @param boolean $public_only
+   * @return array
+   *   An array of schema.org objects keyed by publication categories (e.g.
+   *   News Articles, Books).
+   */
+  protected function getActivityInsightPublications(string $netid, bool $published_only, bool $public_only): array {
+    $cid = 'ai_publication_data:' . $netid;
+
+    if ($cache_data = \Drupal::cache()->get($cid)) {
+      $remote_data = $cache_data->data;
+    }
+    else {
+      // Try to fetch publication data from Activity Insight/Watermark/Digital
+      // Measures.
+      $url = sprintf('https://webservices.digitalmeasures.com/login/service/v4/SchemaData/INDIVIDUAL-ACTIVITIES-IndustrialLaborRelations/USERNAME:%s/INTELLCONT', $netid);
+
+      try {
+        // @todo gzip this request.
+        $response = $this->client->get($url, ['auth' => [getenv('AI_USER'), getenv('AI_PASS')]]);
+      }
+      catch (\Exception $e) {
+        // @todo deal with this. Log it? Throw an error?
+        return [];
+      }
+
+      $remote_data = $response->getBody()->getContents();
+      \Drupal::cache()->set($cid, $remote_data, time() + 60 * 60);
+    }
+
+    $ai_person_xml = new SimpleXMLElement($remote_data);
+    $data = [];
+
+    foreach ($ai_person_xml->Record->INTELLCONT as $publication) {
+      if ($published_only && (string) $publication->STATUS !== 'Published') {
+        continue;
+      }
+
+      if ($public_only && (string) $publication->PUBLIC_VIEW !== 'Yes') {
+        continue;
+      }
+
+      $publication_group = match((string) $publication->CONTYPE) {
+        'Journal Article' => 'Journal Articles',
+        'Book, Scholarly' => 'Books',
+        'Book Chapter' => 'Book Chapters',
+        'Book, Textbook' => 'Textbooks',
+        'Book Section' => 'Book Sections',
+        'Newspaper' => 'Newspapers',
+        'Instructor\'s Manual' => 'Instructor\'s Manuals',
+        'Book Review' => 'Book Reviews',
+        'Magazine Publication' => 'Magazine Publications',
+        'Written Case' => 'Written Cases',
+        'Cited Research' => 'Cited Research',
+        'Conference Proceeding' => 'Conference Proceedings',
+        'Abstract' => 'Abstracts',
+        default => (string) $publication->CONTYPE,
+      };
+
+      switch ($publication_group) {
+        case 'Journal Articles':
+          $schemaObject = Schema::scholarlyArticle()
+            ->publisher(Schema::organization()->name((string) $publication->JOURNAL->JOURNAL_NAME))
+            ->pagination((string) $publication->PAGENUM);
+
+            if ((string) $publication->PAGENUM && (string) $publication->VOLUME) {
+              $schemaObject->isPartOf(
+                Schema::publicationIssue()
+                  ->issueNumber((string) $publication->ISSUE)
+                  ->isPartOf(
+                    Schema::publicationVolume()
+                      ->volumeNumber((string) $publication->VOLUME)
+                  )
+                );
+            }
+          break;
+
+        case 'Books':
+        case 'Textbooks':
+          $schemaObject = Schema::book()
+            ->publisher(Schema::organization()->name((string) $publication->PUBLISHER));
+          break;
+
+        case 'Book Chapters':
+          $schemaObject = Schema::chapter()
+            ->publisher(Schema::organization()->name((string) $publication->PUBLISHER))
+            ->title((string) $publication->BOOK_TITLE);
+          break;
+
+        case 'Newspapers':
+          $schemaObject = Schema::newsArticle();
+          break;
+
+        case 'Book Reviews':
+          $schemaObject = Schema::reviewNewsArticle();
+          break;
+
+        case 'Magazine Publications':
+          $schemaObject = Schema::article();
+          break;
+
+        default:
+          $schemaObject = Schema::creativeWork();
+      }
+
+      $data[$publication_group][] = $schemaObject;
+
+      $authors = [];
+      foreach ($publication->INTELLCONT_AUTH as $author) {
+        $authors[] = Schema::person()
+          ->givenName((string) $author->FNAME)
+          ->familyName((string) $author->LNAME)
+          ->additionalName((string) $author->MNAME);
+      }
+
+      $publication_date = (string) $publication->PUB_START ?: (string) $publication->SUB_START;
+
+      /** @var \Spatie\SchemaOrg\CreativeWork $schemaObject */
+      $schemaObject
+        ->name((string) $publication->TITLE[0])
+        ->author($authors)
+        ->datePublished(new \DateTime($publication_date))
+        ->url((string) $publication->WEB_ADDRESS)
+        ->setProperty('ai_contype', (string) $publication->CONTYPE)
+        ->setProperty('ai_public_view', (string) $publication->PUBLIC_VIEW);
+    }
+
+    return $data;
+  }
+
+}
