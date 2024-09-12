756eebb01085befbf137302d18913f7926276439
Aaron Froehlich <aaronfroehlich@gmail.com>
2024-09-12 11:35:22 -0400

    Add a json feed of public courses and upcoming classes
    Co-authored-by: Jeff Amaral <jeff.amaral@cornell.edu>

---
+++ web/modules/custom/ilr/src/Controller/CourseJsonFeed.php
@@ -0,0 +1,100 @@
+<?php
+
+namespace Drupal\ilr\Controller;
+
+use Drupal\Core\Controller\ControllerBase;
+use Drupal\Core\Datetime\DrupalDateTime;
+use Drupal\datetime\Plugin\Field\FieldType\DateTimeItemInterface;
+use Symfony\Component\HttpFoundation\JsonResponse;
+
+/**
+ * Returns courses as a json feed.
+ */
+class CourseJsonFeed extends ControllerBase {
+
+  /**
+   * The `content` route responds with the feed items.
+   */
+  public function content(): JsonResponse {
+    $data = ['courses' => []];
+    $response = new JsonResponse($data, $status = 200, $headers = []);
+    $node_storage = $this->entityTypeManager()->getStorage('node');
+
+    // Limit to the following topics terms:
+    // Human Resources (7), Leadership Development (8), Employee Relations
+    // and Investigations (3), IDEA (18), Labor Relations (5).
+
+    $course_query = $node_storage->getQuery()
+      ->accessCheck(TRUE)
+      ->condition('type', 'course')
+      ->condition('status', 1)
+      ->sort('title', 'ASC')
+      ->condition('field_topics', [7, 8, 3, 18, 5], 'IN');
+
+    $course_ids = $course_query->execute();
+
+    if (empty($course_ids)) {
+      return $response;
+    }
+
+    $courses = $node_storage->loadMultiple($course_ids);
+
+    foreach ($courses as $course) {
+      // Remove courses without upcoming classes.
+      if (count($course->classes) === 0) {
+        continue;
+      }
+
+      $course_data = [
+        'uuid' => $course->uuid(),
+        'title' => $course->label(),
+        'description' => $course->body->value,
+        'key_takeaways' => $course->field_key_outcomes->value,
+        'audiences' => $course->field_target_audience->value,
+        'topics' => [],
+      ];
+
+      foreach ($course->field_topics->referencedEntities() as $topic) {
+        $course_data['topics'][] = $topic->label();
+      }
+
+      foreach ($course->classes->referencedEntities() as $class) {
+        $delivery_method = ($class->field_delivery_method->value === 'Classroom') ? 'In person' : $class->field_delivery_method->value;
+        $class_start = new DrupalDateTime($class->field_date_start->value, DateTimeItemInterface::STORAGE_TIMEZONE);
+        $register_url = $course->toUrl('canonical', ['absolute' => TRUE]);
+
+        if ($delivery_method == 'In person') {
+          $location = strtr("%address_line1\n%city, %state", [
+            '%address_line1' => $class->field_address->address_line1,
+            '%city' => $class->field_address->locality,
+            '%state' => $class->field_address->administrative_area,
+          ]);
+        }
+
+        if ($mapped_object = $class->getClassNodeSalesforceMappedObject()) {
+          $register_url = \Drupal::configFactory()->get('ilr_registration_system.settings')->get('url') . $mapped_object->salesforce_id->getString();
+        }
+
+        if (!$class->field_external_link->isEmpty()) {
+          $register_url = $class->field_external_link->uri;
+        }
+
+        $course_data['sessions'][] = [
+          'start' => $class_start->format('c'),
+          'location' => $location,
+          'delivery_type' => $delivery_method,
+          'price' => '$' . $class->field_price->value,
+          'discount_price' => '$' . ($class->ilroutreach_discount_price->value ?? $class->field_price->value),
+          'discount_enddate' => $class->ilroutreach_discount_date->end_value,
+          'registration_url' => $register_url,
+        ];
+      }
+
+      $data['courses'][] = $course_data;
+    }
+
+    $response->setData($data);
+    return $response;
+  }
+
+}
