01b81e6266cb282493a1f617d3313e24a25bdb1f
Jeff Amaral <jeff.amaral@cornell.edu>
2024-12-02 18:09:46 -0500

    Add a search filter to the person admin listing

--- web/modules/custom/person/src/PersonListBuilder.php
+++ web/modules/custom/person/src/PersonListBuilder.php
@@ -4,7 +4,12 @@ namespace Drupal\person;
 
 use Drupal\Core\Entity\EntityInterface;
 use Drupal\Core\Entity\EntityListBuilder;
+use Drupal\Core\Entity\EntityStorageInterface;
+use Drupal\Core\Entity\EntityTypeInterface;
+use Drupal\Core\Entity\Query\QueryInterface;
 use Drupal\Core\Url;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use Symfony\Component\HttpFoundation\Request;
 
 /**
  * Defines a class to build a listing of Person entities.
@@ -12,6 +17,28 @@ use Drupal\Core\Url;
  * @ingroup person
  */
 class PersonListBuilder extends EntityListBuilder {
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function createInstance(ContainerInterface $container, EntityTypeInterface $entity_type) {
+    return new static(
+      $entity_type,
+      $container->get('entity_type.manager')->getStorage($entity_type->id()),
+      $container->get('request_stack')->getCurrentRequest()
+    );
+  }
+
+  /**
+   * Constructs a new PersonListBuilder object.
+   */
+  public function __construct(
+    EntityTypeInterface $entityType,
+    EntityStorageInterface $storage,
+    protected Request $request
+  ) {
+    parent::__construct($entityType, $storage);
+  }
 
   /**
    * {@inheritdoc}
@@ -32,6 +59,42 @@ class PersonListBuilder extends EntityListBuilder {
       '#url' => Url::fromRoute('entity.persona.collection', ['person' => $entity->id()]),
     ];
     return $row + parent::buildRow($entity);
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function render() {
+    // Add our custom filter form to the build.
+    $build['form'] = \Drupal::formBuilder()->getForm('Drupal\person\Form\PersonCollectionSearchForm');
+    $build += parent::render();
+
+    // Change the empty table message from EntityListBuilder::render().
+    $build['table']['#empty'] = $this->t('No @label found.', ['@label' => $this->entityType->getPluralLabel()]);
+
+    return $build;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function getEntityListQuery(): QueryInterface {
+    // We don't call $query = parent:: getEntityListQuery() because it adds a
+    // sort on id, and we want our own sort on display_name.
+    $query = $this->getStorage()->getQuery()
+      ->accessCheck(TRUE)
+      ->sort('display_name');
+
+    // Only add the pager if a limit is specified.
+    if ($this->limit) {
+      $query->pager($this->limit);
+    }
+
+    if ($name = $this->request->get('name')) {
+      $query->condition('display_name', $name, 'CONTAINS');
+    }
+
+    return $query;
   }
 
   /**
