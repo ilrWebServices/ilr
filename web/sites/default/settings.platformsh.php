b9df629b6b247b4ebcb0be7adbc93326dc84b77f
Jeff Amaral <jeff.amaral@cornell.edu>
2025-02-05 11:47:19 -0500

    Upgrade platformsh/config-reader with dependencies

--- web/sites/default/settings.platformsh.php
+++ web/sites/default/settings.platformsh.php
@@ -18,7 +18,10 @@ if ($platformsh->hasRelationship('database')) {
     'password' => $creds['password'],
     'host' => $creds['host'],
     'port' => $creds['port'],
-    'pdo' => [PDO::MYSQL_ATTR_COMPRESS => !empty($creds['query']['compression'])]
+    'pdo' => [PDO::MYSQL_ATTR_COMPRESS => !empty($creds['query']['compression'])],
+    'init_commands' => [
+      'isolation_level' => 'SET SESSION TRANSACTION ISOLATION LEVEL READ COMMITTED',
+    ],
   ];
 }
 
@@ -27,7 +30,7 @@ if ($platformsh->hasRelationship('database')) {
 // on development but not production.
 if (isset($platformsh->branch)) {
   // Production type environment.
-  if ($platformsh->branch == 'master' || $platformsh->onDedicated()) {
+  if ($platformsh->onProduction() || $platformsh->onDedicated()) {
     $config['system.logging']['error_level'] = 'hide';
   } // Development type environment.
   else {
@@ -128,26 +131,26 @@ foreach ($platformsh->variables() as $name => $value) {
   $parts = explode(':', $name);
   list($prefix, $key) = array_pad($parts, 3, null);
   switch ($prefix) {
-    // Variables that begin with `d8settings` or `drupal` get mapped
+    // Variables that begin with `drupalsettings` or `drupal` get mapped
     // to the $settings array verbatim, even if the value is an array.
-    // For example, a variable named d8settings:example-setting' with
+    // For example, a variable named drupalsettings:example-setting' with
     // value 'foo' becomes $settings['example-setting'] = 'foo';
     case 'drupalsettings':
     case 'drupal':
       $settings[$key] = $value;
       break;
-    // Variables that begin with `d8config` get mapped to the $config
+    // Variables that begin with `drupalconfig` get mapped to the $config
     // array.  Deeply nested variable names, with colon delimiters,
     // get mapped to deeply nested array elements. Array values
     // get added to the end just like a scalar. Variables without
     // both a config object name and property are skipped.
-    // Example: Variable `d8config:conf_file:prop` with value `foo` becomes
+    // Example: Variable `drupalconfig:conf_file:prop` with value `foo` becomes
     // $config['conf_file']['prop'] = 'foo';
-    // Example: Variable `d8config:conf_file:prop:subprop` with value `foo` becomes
+    // Example: Variable `drupalconfig:conf_file:prop:subprop` with value `foo` becomes
     // $config['conf_file']['prop']['subprop'] = 'foo';
-    // Example: Variable `d8config:conf_file:prop:subprop` with value ['foo' => 'bar'] becomes
+    // Example: Variable `drupalconfig:conf_file:prop:subprop` with value ['foo' => 'bar'] becomes
     // $config['conf_file']['prop']['subprop']['foo'] = 'bar';
-    // Example: Variable `d8config:prop` is ignored.
+    // Example: Variable `drupalconfig:prop` is ignored.
     case 'drupalconfig':
       if (count($parts) > 2) {
         $temp = &$config[$key];
