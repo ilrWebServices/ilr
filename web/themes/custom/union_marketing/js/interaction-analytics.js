7fae31d6e8549a9b68023881fed11df705c34bc1
Aaron Froehlich <aaronfroehlich@gmail.com>
2025-02-07 17:17:48 -0500

    Add a webform handler that supports populating the datalayer
    ILR-1561

--- web/themes/custom/union_marketing/js/interaction-analytics.js
+++ web/themes/custom/union_marketing/js/interaction-analytics.js
@@ -11,14 +11,19 @@
    */
   Drupal.behaviors.interactions = {
     attach: function (context, settings) {
-      // Only run on full page requests, not ajax.
-      if (context !== document) {
+      // Only run on full page requests, unless specified.
+      if (context !== document && !settings.include_ajax) {
         return;
       }
 
-      if (settings.content_attributes) {
+      if (context == document && settings.content_attributes) {
         window.dataLayer = window.dataLayer || [];
         dataLayer.push(settings.content_attributes);
+      }
+
+      if (settings.webform_data) {
+        window.dataLayer = window.dataLayer || [];
+        dataLayer.push(settings.webform_data);
       }
     }
   }
