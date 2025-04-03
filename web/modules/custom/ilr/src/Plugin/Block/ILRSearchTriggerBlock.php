70ede5753ac1f2b3e19bd7dc7ffa69624035e751
Jeff Amaral <jeff.amaral@cornell.edu>
2025-04-03 16:49:12 -0400

    Refactor site header and include utility nav
    Co-authored-by: Aaron Froehlich <aaronfroehlich@gmail.com>

--- web/modules/custom/ilr/src/Plugin/Block/ILRSearchTriggerBlock.php
+++ web/modules/custom/ilr/src/Plugin/Block/ILRSearchTriggerBlock.php
@@ -27,7 +27,11 @@ class ILRSearchTriggerBlock extends BlockBase {
    */
   public function build() {
     $build = [
-      '#theme' => 'ilr_search_trigger_block',
+      '#type' => 'inline_template',
+      '#template' => '<form class="ilr-search" action="/search" method="GET"><input type="text" name="s" placeholder="{% trans %}Search{% endtrans %}" value="{{ value }}"/><button aria-label="Search" title="Search"><svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" height="24" viewBox="0 0 24 24" width="24" focusable="false" aria-hidden="true" style="pointer-events: none; display: inherit; width: 100%; height: 100%;"><path clip-rule="evenodd" d="M16.296 16.996a8 8 0 11.707-.708l3.909 3.91-.707.707-3.909-3.909zM18 11a7 7 0 00-14 0 7 7 0 1014 0z" fill-rule="evenodd"></path></svg></button></form>',
+      '#context' => [
+        'value' => '',
+      ],
     ];
 
     return $build;
