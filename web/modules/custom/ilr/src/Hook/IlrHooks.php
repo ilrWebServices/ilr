9fb762b02d6bbb5960d0573cf038f42ac8b8407c
Jeff Amaral <jeff.amaral@cornell.edu>
2025-05-15 14:15:01 -0400

    Add a missing representative image warning when saving nodes
    Co-authored-by: Aaron Froehlich <aaronfroehlich@gmail.com>

--- web/modules/custom/ilr/src/Hook/IlrHooks.php
+++ web/modules/custom/ilr/src/Hook/IlrHooks.php
@@ -4,8 +4,11 @@ namespace Drupal\ilr\Hook;
 
 use Drupal\Core\Entity\ContentEntityInterface;
 use Drupal\Core\Hook\Attribute\Hook;
+use Drupal\Core\StringTranslation\StringTranslationTrait;
 
 class IlrHooks {
+
+  use StringTranslationTrait;
 
   #[Hook('node_presave')]
   public function populateRepresentativeImage(ContentEntityInterface $entity) {
@@ -19,6 +22,17 @@ class IlrHooks {
 
     if (!$entity->field_featured_media->isEmpty() && $entity->field_featured_media->entity->bundle() === 'image' && $entity->field_representative_image->isEmpty()) {
       $entity->field_representative_image = $entity->field_featured_media->target_id;
+    }
+  }
+
+  #[Hook('node_insert')]
+  #[Hook('node_update')]
+  public function missingRepresetativeImageNotice(ContentEntityInterface $entity) {
+    if ($entity->hasField('field_representative_image') && $entity->field_representative_image->isEmpty()) {
+      $message = $this->t('This %entity_type is missing a representative image. While not required, please note that this will prevent the content from appearing in some listings and will behave unpredictably in search results and social sharing links.', [
+        '%entity_type' => $entity->type->entity->label()
+      ]);
+      \Drupal::messenger()->addWarning($message);
     }
   }
 
