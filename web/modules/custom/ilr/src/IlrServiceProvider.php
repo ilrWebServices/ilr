8ad673606c1a6885cdd878a56d744b70686f5658
Jeff Amaral <jeff.amaral@cornell.edu>
2025-07-16 16:11:44 -0400

    Add a new log handler for 'page not found' errors
    Note that this monolog handler _does not_ also log to drupal.dblog. This will prevent 404 errors from appearing in our general error log. This is desired.
    
    The Grist document that receives these errors will be able to aggregate missing pages and help us better manage missing pages in real time.
    
    Co-authored-by: Aaron Froehlich <aaronfroehlich@gmail.com>

--- web/modules/custom/ilr/src/IlrServiceProvider.php
+++ web/modules/custom/ilr/src/IlrServiceProvider.php
@@ -25,6 +25,13 @@ class IlrServiceProvider extends ServiceProviderBase {
       $definition = $container->getDefinition('monolog.handler.slack_webhook');
       $definition->setArgument('$webhookUrl', getenv('SLACK_NOTIFICATIONS_WEBHOOK_URL'));
     }
+
+    // Overrides the grist_link_log related arguments in sites/default/monolog.services.yml.
+    if ($container->hasDefinition('monolog.handler.grist_link_log')) {
+      $definition = $container->getDefinition('monolog.handler.grist_link_log');
+      $definition->setArgument('$gristDocument', getenv('GRIST_LINK_LOG_DOCUMENT_URL'));
+      $definition->setArgument('$gristApiToken', getenv('GRIST_LINK_LOG_API_TOKEN'));
+    }
   }
 
 }
