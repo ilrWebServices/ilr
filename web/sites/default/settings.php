54e1db1464cca61e0783e98642a0141bc0955192
Joe Verrastro <joe@zensource.cloud>
2025-09-29 13:25:19 -0400

    syncing local environment changes

--- web/sites/default/settings.php
+++ web/sites/default/settings.php
@@ -1061,3 +1061,9 @@ $config['migrate_scheduler']['migrations'] = [
 if (file_exists($app_root . '/' . $site_path . '/settings.local.php')) {
   include $app_root . '/' . $site_path . '/settings.local.php';
 }
+
+// Automatically generated include for settings managed by ddev.
+$ddev_settings = __DIR__ . '/settings.ddev.php';
+if (getenv('IS_DDEV_PROJECT') == 'true' && is_readable($ddev_settings)) {
+  require $ddev_settings;
+}
