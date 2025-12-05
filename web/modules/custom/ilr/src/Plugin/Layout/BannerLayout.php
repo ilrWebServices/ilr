eae9717f7507f0d3770600642c12960b940b5599
gregwarren <greg.warren@primacy.com>
2025-12-05 16:36:57 -0500

    Add new hide frame, text align, and full bleed settings to Banner layout
    Co-authored-by: Jeff Amaral <jeff.amaral@cornell.edu>

--- web/modules/custom/ilr/src/Plugin/Layout/BannerLayout.php
+++ web/modules/custom/ilr/src/Plugin/Layout/BannerLayout.php
@@ -17,6 +17,9 @@ class BannerLayout extends LayoutDefault implements PluginFormInterface {
   public function defaultConfiguration() {
     return parent::defaultConfiguration() + [
       'color_scheme' => 'dark',
+      'hide_frame' => FALSE,
+      'text_align' => 'left',
+      'full_bleed' => FALSE,
     ];
   }
 
@@ -39,6 +42,30 @@ class BannerLayout extends LayoutDefault implements PluginFormInterface {
       '#default_value' => $configuration['color_scheme'],
     ];
 
+    $form['hide_frame'] = [
+      '#type' => 'checkbox',
+      '#title' => $this->t('Hide frame'),
+      '#description' => $this->t('Remove the frame/border around the banner.'),
+      '#default_value' => $configuration['hide_frame'],
+    ];
+
+    $form['text_align'] = [
+      '#type' => 'select',
+      '#title' => $this->t('Text alignment'),
+      '#options' => [
+        'left' => 'Left',
+        'center' => 'Center',
+      ],
+      '#default_value' => $configuration['text_align'],
+    ];
+
+    $form['full_bleed'] = [
+      '#type' => 'checkbox',
+      '#title' => $this->t('Full bleed'),
+      '#description' => $this->t('Extend the banner to the full width of the viewport.'),
+      '#default_value' => $configuration['full_bleed'],
+    ];
+
     return $form;
   }
 
@@ -55,6 +82,9 @@ class BannerLayout extends LayoutDefault implements PluginFormInterface {
    */
   public function submitConfigurationForm(array &$form, FormStateInterface $form_state) {
     $this->configuration['color_scheme'] = $form_state->getValue('color_scheme');
+    $this->configuration['hide_frame'] = (bool) $form_state->getValue('hide_frame');
+    $this->configuration['text_align'] = $form_state->getValue('text_align');
+    $this->configuration['full_bleed'] = (bool) $form_state->getValue('full_bleed');
   }
 
 }
