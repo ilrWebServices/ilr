eece2abc587f4f26a31ccd2d5a94e21cc641b7f0
Jeff Amaral <jeff.amaral@cornell.edu>
2025-12-12 16:15:09 -0500

    Add deploy hook to delete the milr_visit_form_submission_lead salesforce mapping
    Even though the mapping was removed from config, our config ignore pattern module ignores the removal and leaves the mapping behind in the database.
    
    This also removes the mapped objects that would otherwise have been orphaned.
    
    It retains the 'mapped' salesforce object reference in the key_value store, which shows up in the notes of the previous submissions.

--- web/modules/custom/ilr/ilr.deploy.php
+++ web/modules/custom/ilr/ilr.deploy.php
@@ -257,3 +257,24 @@ function ilr_deploy_transform_grad_visit_request_mapped_objects(&$sandbox) {
     $mapped_object->delete();
   }
 }
+
+/**
+ * Delete milr_visit_form_submission_lead salesforce mapping and mapped objects.
+ */
+function ilr_deploy_milr_visit_form_submission_lead_mapping_delete(&$sandbox) {
+  $entity_type_manager = \Drupal::service('entity_type.manager');
+  $mapping = $entity_type_manager->getStorage('salesforce_mapping')->load('milr_visit_form_submission_lead');
+  $mapped_object_storage = $entity_type_manager->getStorage('salesforce_mapped_object');
+  $sfDataStore = \Drupal::service('keyvalue')->get('ilr_salesforce.touchpoint.sfid');
+  $mapped_objects = $mapped_object_storage->loadByProperties(['salesforce_mapping' => [$mapping->id()]]);
+
+  foreach ($mapped_objects as $mapped_object) {
+    $webform_submission = $mapped_object->getMappedEntity();
+    $sfDataStore->set($webform_submission->id(), $mapped_object->sfid());
+    $mapped_object->delete();
+  }
+
+  // This is necessary, even though the config files have been removed,
+  // because the mapping config depends on ignored webform config.
+  $mapping->delete();
+}
