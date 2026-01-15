69ed74e54ef1128f390fae0aff2530de4b34762a
Aaron Froehlich <aaronfroehlich@gmail.com>
2026-01-15 10:36:13 -0500

    Add ExtraField Display for delivery method on events and begin styling
    This transforms the data from Salesforce to display in a user friendly way, while also preserving modifications an editor might make.
    
    Note that we render 'Online' by default to avoid an empty wrapper. 
    See https://www.drupal.org/i/3152281

--- web/modules/custom/ilr/src/Entity/EventLandingNode.php
+++ web/modules/custom/ilr/src/Entity/EventLandingNode.php
@@ -4,4 +4,42 @@ namespace Drupal\ilr\Entity;
 
 class EventLandingNode extends EventNodeBase implements EventNodeInterface {
 
+  /**
+   * Gets a string representation of the event delivery method.
+   *
+   * @return string
+   *  A translated string.
+   */
+  public function deliveryMethod():string {
+    if ($this->hasField('field_delivery_method')) {
+      $delivery_method = $this->field_delivery_method->value;
+
+      switch ($delivery_method) {
+        case 'In Person':
+          $delivery_method = 'In Person';
+          break;
+        case 'Online (Synchronous)':
+          $delivery_method = 'Online';
+          break;
+        case 'Online (Date Driven)':
+          $delivery_method = 'Online Series';
+          break;
+        case 'On Demand/Self Paced':
+          $delivery_method = 'On Demand';
+          break;
+      }
+    }
+
+    if (empty($delivery_method)) {
+      if (!$this->location_address->isEmpty()) {
+        $delivery_method = 'In Person';
+      }
+
+      if (!$this->location_link->isEmpty()) {
+        $delivery_method = 'Online';
+      }
+    }
+
+    return $delivery_method ?? 'Online';
+  }
 }
