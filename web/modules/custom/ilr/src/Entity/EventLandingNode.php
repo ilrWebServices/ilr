2d8dca71a270dd474b8b9a6c0795146f15c5b839
Aaron Froehlich <aaronfroehlich@gmail.com>
2026-01-22 09:58:45 -0500

    Update how delivery methods are calculated on event landing nodes
    - Simplify "Online" label from Salesforce
    - No longer assume that the location link implies Online

--- web/modules/custom/ilr/src/Entity/EventLandingNode.php
+++ web/modules/custom/ilr/src/Entity/EventLandingNode.php
@@ -8,38 +8,38 @@ class EventLandingNode extends EventNodeBase implements EventNodeInterface {
    * Gets a string representation of the event delivery method.
    *
    * @return string
-   *  A translated string.
+   *   The delivery method.
    */
   public function deliveryMethod():string {
     if ($this->hasField('field_delivery_method')) {
       $delivery_method = $this->field_delivery_method->value;
 
+      // Normalize certain values from SalesForce, but do not provide a default
+      // (that way, we won't accidentally override a saved field value).
       switch ($delivery_method) {
         case 'In Person':
           $delivery_method = 'In Person';
           break;
+
         case 'Online (Synchronous)':
+        case 'Online (Date Driven)':
           $delivery_method = 'Online';
           break;
-        case 'Online (Date Driven)':
-          $delivery_method = 'Online Series';
-          break;
+
         case 'On Demand/Self Paced':
           $delivery_method = 'On Demand';
           break;
       }
     }
 
+    // If still no value, then check the address field.
     if (empty($delivery_method)) {
       if (!$this->location_address->isEmpty()) {
         $delivery_method = 'In Person';
-      }
-
-      if (!$this->location_link->isEmpty()) {
-        $delivery_method = 'Online';
       }
     }
 
     return $delivery_method ?? 'Online';
   }
+
 }
