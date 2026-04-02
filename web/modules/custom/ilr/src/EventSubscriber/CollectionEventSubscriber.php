1c08b6a7fa97edbda7231831e38fc5da2dd8dcef
Aaron Froehlich <aaronfroehlich@gmail.com>
2026-04-02 16:30:53 -0400

    Update posts and term intro text to use simple formatting

--- web/modules/custom/ilr/src/EventSubscriber/CollectionEventSubscriber.php
+++ web/modules/custom/ilr/src/EventSubscriber/CollectionEventSubscriber.php
@@ -191,8 +191,8 @@ class CollectionEventSubscriber implements EventSubscriberInterface {
           'field_storage' => FieldStorageConfig::loadByName('taxonomy_term', 'field_body'),
           'third_party_settings' => [
             'allowed_formats' => [
-              'basic_formatting' => 'basic_formatting',
-              'basic_formatting_with_media' => '0',
+              'simple_formatting' => 'simple_formatting',
+              'standard_formatting' => '0',
               'full_html' => '0',
               'inline_svg' => '0',
               'plain_text' => '0',
