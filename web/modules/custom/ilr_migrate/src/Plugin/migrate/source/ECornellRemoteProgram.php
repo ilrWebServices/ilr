675dc5360bdcd38ed06615816d5cd5117e348879
Jeff Amaral <jeff.amaral@cornell.edu>
2026-06-16 17:31:55 -0400

    Add a new remote_program content type
    This should be similar to course nodes, with identically named date, etc. fields so that it can be used in course listings and the upcoming program finder.

---
+++ web/modules/custom/ilr_migrate/src/Plugin/migrate/source/ECornellRemoteProgram.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Drupal\ilr_migrate\Plugin\migrate\source;
+
+use Drupal\Component\Plugin\ConfigurableInterface;
+use Drupal\migrate\Plugin\migrate\source\SourcePluginBase;
+
+/**
+ * Custom source fabricated from calls to the eCornell API.
+ *
+ * @MigrateSource(
+ *   id = "ecornell_remote_program",
+ *   source_module = "ilr_migrate"
+ * )
+ */
+class ECornellRemoteProgram extends SourcePluginBase implements ConfigurableInterface {
+
+
+
+}
