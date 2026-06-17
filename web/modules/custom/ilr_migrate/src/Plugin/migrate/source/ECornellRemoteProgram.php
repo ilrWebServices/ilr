07dff8d731e0c7ad360914fe99f5cdeffbdd7feb
Jeff Amaral <jeff.amaral@cornell.edu>
2026-06-17 11:18:40 -0400

    WIP: Begin custom eCornell remote program migrate source plugin
    This will use the eCornell API to fetch Certificate data, process it, and present it as iterable data for a custom migration that can run on a schedule.

--- web/modules/custom/ilr_migrate/src/Plugin/migrate/source/ECornellRemoteProgram.php
+++ web/modules/custom/ilr_migrate/src/Plugin/migrate/source/ECornellRemoteProgram.php
@@ -2,19 +2,84 @@
 
 namespace Drupal\ilr_migrate\Plugin\migrate\source;
 
-use Drupal\Component\Plugin\ConfigurableInterface;
 use Drupal\migrate\Plugin\migrate\source\SourcePluginBase;
+use Drupal\migrate\Plugin\MigrationInterface;
+use GuzzleHttp\ClientInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
  * Custom source fabricated from calls to the eCornell API.
  *
  * @MigrateSource(
  *   id = "ecornell_remote_program",
- *   source_module = "ilr_migrate"
  * )
  */
-class ECornellRemoteProgram extends SourcePluginBase implements ConfigurableInterface {
+class ECornellRemoteProgram extends SourcePluginBase {
 
+  /**
+   * {@inheritdoc}
+   */
+  public function __construct(
+    array $configuration,
+    $plugin_id,
+    $plugin_definition,
+    MigrationInterface $migration,
+    protected ClientInterface $httpClient
+  ) {
+    parent::__construct($configuration, $plugin_id, $plugin_definition, $migration);
+  }
 
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition, ?MigrationInterface $migration = NULL) {
+    return new static(
+      $configuration,
+      $plugin_id,
+      $plugin_definition,
+      $migration,
+      $container->get('http_client')
+    );
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function fields(): array {
+    return [
+      'id' => $this->t('Certificate ID'),
+      'title' => $this->t('Certificate Title'),
+      'description' => $this->t('Certificate Description'),
+      'code' => $this->t('Certificate Code'),
+      'delivery_method' => $this->t('Certificate Delivery Method'),
+      'next_course_start_date' => $this->t('Next Course Start Date'),
+    ];
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getIds(): array {
+    return [
+      'id' => [
+        'type' => 'string',
+      ],
+    ];
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function initializeIterator(): \Iterator {
+    $data = [];
+    return new \ArrayIterator($data);
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function __toString() {
+    return 'External eCornell Certificates';
+  }
 
 }
